6c2fa5bb48504d20fa8908d13275c23587de9cc0
Emily Lockhart <validitymaiden@gmail.com>
2023-07-27 13:20:27 -0400

    9 squares in a fragment

--- app/ttt/page.js
+++ app/ttt/page.js
@@ -1,5 +1,17 @@
 import styles from './styles.css'
 
 export default function Square() {
-    return <button className="square">X</button>;
+    return (
+        <>
+            <button className="square">X</button>
+            <button className="square">X</button>
+            <button className="square">X</button>
+            <button className="square">X</button>
+            <button className="square">X</button>
+            <button className="square">X</button>
+            <button className="square">X</button>
+            <button className="square">X</button>
+            <button className="square">X</button>
+        </>
+    );
 }
