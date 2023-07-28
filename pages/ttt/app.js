4350a581d0ca1496d170fdf1463466f5b37e0c7b
Emily Lockhart <validitymaiden@gmail.com>
2023-07-28 13:00:10 -0400

    move turn and board state into Game and pass state to Board with props make click functionality add the board state to a history and have Game pass Board the most current board state

--- pages/ttt/app.js
+++ pages/ttt/app.js
@@ -8,10 +8,7 @@ function Square({value, onSquareClick}) {
     )
 }
 
-function Board() {
-    const [xIsNext, setXIsNext] = useState(true)
-    const [squares, setSquares] = useState(Array(9).fill(null))
-
+function Board({ xIsNext, squares, onPlay }) {
     const winner = calculateWinner(squares);
     let status
     if (winner) {
@@ -29,8 +26,7 @@ function Board() {
             } else {
                 nextSquares[i] = 'O'
             }
-            setSquares(nextSquares)
-            setXIsNext(!xIsNext)    
+            onPlay(nextSquares)
         }
     }
 
@@ -77,10 +73,19 @@ function calculateWinner(squares) {
 }
 
 export default function Game() {
+    const [xIsNext, setXIsNext] = useState(true)
+    const [history, setHistory] = useState([Array(9).fill(null)])
+    const currentSquares = history[history.length - 1]
+
+    function handlePlay(nextSquares) {
+        setHistory([...history, nextSquares])
+        setXIsNext(!xIsNext)
+    }
+
     return (
         <div className='game'>
             <div className='game-board'>
-                <Board/>
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
             </div>
             <div className='game-info'>
                 <ol>{/*TODO*/}</ol>
