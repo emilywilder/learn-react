b32c289aab809c9389eb6f891a3c0ce2b8bc288f
Emily Lockhart <validitymaiden@gmail.com>
2023-08-24 11:26:27 -0400

    pass functions to update state through props to children

--- pages/product-stock/app.js
+++ pages/product-stock/app.js
@@ -68,17 +68,26 @@ function ProductTable({ products, filterText, inStockOnly }) {
     )
 }
 
-function SearchBar({ filterText, inStockOnly }) {
+function SearchBar({ 
+    filterText,
+    inStockOnly,
+    onFilterTextChange,
+    onInStockOnlyChange
+}) {
     return (
         <form>
             <input
                 type="text"
                 value={filterText}
-                placeholder="Search..." />
+                placeholder="Search..."
+                onChange={(e) => onFilterTextChange(e.target.value)}
+            />
             <label>
                 <input
                     type="checkbox"
-                    checked={inStockOnly} />
+                    checked={inStockOnly}
+                    onChange={(e) => onInStockOnlyChange(e.target.checked)}
+                />
                 {' '}
                 Only show products in stock
             </label>
@@ -94,7 +103,10 @@ function FilterableProductTable({ products }) {
         <div>
             <SearchBar
                 filterText={filterText}
-                inStockOnly={inStockOnly} />
+                inStockOnly={inStockOnly}
+                onFilterTextChange={setFilterText}
+                onInStockOnlyChange={setInStockOnly}
+            />
             <ProductTable
                 products={products}
                 filterText={filterText}
