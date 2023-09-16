448a00b661124937f9d465d21e80496416f2bc54
Emily Lockhart <validitymaiden@gmail.com>
2023-09-16 15:23:25 -0400

    your first component challenge 2 solution

--- pages/learn-react/describe-ui/first-comp/Challenge-2.js
+++ pages/learn-react/describe-ui/first-comp/Challenge-2.js
@@ -1,5 +1,6 @@
 export default function Profile() {
-    return
-      <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />;
+    return (
+        <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />
+    )
   }
   
