418184e651115a4a2762940d9ad3ae855e23ed8b
Emily Lockhart <validitymaiden@gmail.com>
2023-09-16 15:19:54 -0400

    add first challenge question for your first component

---
+++ pages/learn-react/describe-ui/first-comp/Challenge-1.js
@@ -0,0 +1,11 @@
+export { Profile as Challenge1 }
+
+function Profile() {
+    return (
+      <img
+        src="https://i.imgur.com/lICfvbD.jpg"
+        alt="Aklilu Lemma"
+      />
+    );
+  }
+  
--- pages/learn-react/describe-ui/first-comp/index.js
+++ pages/learn-react/describe-ui/first-comp/index.js
@@ -2,11 +2,12 @@ import React, { StrictMode } from "react";
 import "./styles.css";
 
 import App from './App.js'
+import Challenge1 from './Challenge-1.js'
 
 export default function Home() {
     return (
         <StrictMode>
-            <App />
+            <Challenge1 />
         </StrictMode>
     )
 }
