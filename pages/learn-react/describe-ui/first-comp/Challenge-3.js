b87db1f5ae8e215de95fa39e8763ca3f21a5a05d
Emily Lockhart <validitymaiden@gmail.com>
2023-09-16 15:24:28 -0400

    add your first component challenge 3

---
+++ pages/learn-react/describe-ui/first-comp/Challenge-3.js
@@ -0,0 +1,20 @@
+function profile() {
+    return (
+      <img
+        src="https://i.imgur.com/QIrZWGIs.jpg"
+        alt="Alan L. Hart"
+      />
+    );
+  }
+  
+  export default function Gallery() {
+    return (
+      <section>
+        <h1>Amazing scientists</h1>
+        <profile />
+        <profile />
+        <profile />
+      </section>
+    );
+  }
+  
--- pages/learn-react/describe-ui/first-comp/index.js
+++ pages/learn-react/describe-ui/first-comp/index.js
@@ -4,11 +4,12 @@ import "./styles.css";
 import App from './App.js'
 import Challenge1 from './Challenge-1.js'
 import Challenge2 from './Challenge-2.js'
+import Challenge3 from './Challenge-3.js'
 
 export default function Home() {
     return (
         <StrictMode>
-            <Challenge2 />
+            <Challenge3 />
         </StrictMode>
     )
 }
