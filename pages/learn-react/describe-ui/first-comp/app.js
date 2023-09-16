d16737f1e39d1b28a6e3bc62f65852bd458f7a5d
Emily Lockhart <validitymaiden@gmail.com>
2023-09-16 14:55:56 -0400

    tile profiles utilizing components

--- pages/learn-react/describe-ui/first-comp/app.js
+++ pages/learn-react/describe-ui/first-comp/app.js
@@ -1,10 +1,21 @@
-export { Profile as App }
+export { Gallery as App }
 
-export default function Profile () {
+function Profile () {
     return (
         <img
             src="https://i.imgur.com/MK3eW3Am.jpg"
             alt="Katherine Johnson"
         />
+    )
+}
+
+export default function Gallery () {
+    return (
+        <section>
+            <h1>Amazing scientists</h1>
+            <Profile />
+            <Profile />
+            <Profile />
+        </section>
     )
 }
