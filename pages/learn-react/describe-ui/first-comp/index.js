0c9a99cba55bb405b8edd9fac92d263e2cab25ab
Emily Lockhart <validitymaiden@gmail.com>
2023-09-16 15:22:51 -0400

    add your first component challenge 2

--- pages/learn-react/describe-ui/first-comp/index.js
+++ pages/learn-react/describe-ui/first-comp/index.js
@@ -3,11 +3,12 @@ import "./styles.css";
 
 import App from './App.js'
 import Challenge1 from './Challenge-1.js'
+import Challenge2 from './Challenge-2.js'
 
 export default function Home() {
     return (
         <StrictMode>
-            <Challenge1 />
+            <Challenge2 />
         </StrictMode>
     )
 }
