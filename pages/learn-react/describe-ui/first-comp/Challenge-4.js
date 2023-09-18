00c40f09260903104c382efa68bd6de153a121ae
Emily Lockhart <validitymaiden@gmail.com>
2023-09-18 12:58:54 -0400

    your first component challenge 4 solution

--- pages/learn-react/describe-ui/first-comp/Challenge-4.js
+++ pages/learn-react/describe-ui/first-comp/Challenge-4.js
@@ -1,2 +1,27 @@
 // Write your component below!
 
+export { Congratulations as Challenge4 }
+
+function YouDidIt() {
+    return (
+        <img
+            src="https://i.kym-cdn.com/photos/images/newsfeed/001/569/889/210.gif"
+            alt="You did it"    
+        />
+    )
+}
+
+function GoodJob() {
+    return (
+        <h1>Good job!</h1>
+    )
+}
+
+export default function Congratulations() {
+    return (
+        <>
+            <GoodJob />
+            <YouDidIt />
+        </>
+    )
+}
