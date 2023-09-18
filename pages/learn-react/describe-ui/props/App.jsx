cc303ae6220d058b5483843de6f2d78d7673437d
Emily Lockhart <validitymaiden@gmail.com>
2023-09-18 16:34:48 -0400

    demonstrate using props to show three profiles with varying people and sizes

--- pages/learn-react/describe-ui/props/App.jsx
+++ pages/learn-react/describe-ui/props/App.jsx
@@ -14,9 +14,28 @@ function Avatar({ person, size }) {
 
 export default function Profile() {
     return (
-        <Avatar 
-            person={ { name: 'Lin Lanying', imageId: '1bX5QH6' } }
-            size={100}
-        />
+        <div>
+            <Avatar
+                size={100}
+                person={{
+                    name: 'Katsuko Saruhashi',
+                    imageId: 'YfeOqp2'
+                }}
+            />
+            <Avatar
+                size={80}
+                person={{
+                    name: 'Aklilu Lemma',
+                    imageId: 'OKS67lh'
+                }}
+            />
+            <Avatar
+                size={50}
+                person={{
+                    name: 'Lin Lanying',
+                    imageId: '1bX5QH6'
+                }}
+            />
+        </div>
     )
 }
