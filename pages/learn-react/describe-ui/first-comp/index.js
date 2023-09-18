8c534e0563cddcad56a637f8920b803794975366
Emily Lockhart <validitymaiden@gmail.com>
2023-09-18 12:45:22 -0400

    your first component challenge 4

--- pages/learn-react/describe-ui/first-comp/index.js
+++ pages/learn-react/describe-ui/first-comp/index.js
@@ -5,11 +5,12 @@ import App from './App.js'
 import Challenge1 from './Challenge-1.js'
 import Challenge2 from './Challenge-2.js'
 import Challenge3 from './Challenge-3.js'
+import Challenge4 from './Challenge-4.js'
 
 export default function Home() {
     return (
         <StrictMode>
-            <Challenge3 />
+            <Challenge4 />
         </StrictMode>
     )
 }
