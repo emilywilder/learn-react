81284a29211656593e8110c12269231208c84aee
Emily Lockhart <validitymaiden@gmail.com>
2023-09-18 13:53:22 -0400

    add jsx-markup with initial example

---
+++ pages/learn-react/describe-ui/jsx-markup/index.js
@@ -0,0 +1,12 @@
+import React, { StrictMode } from "react";
+import "./styles.css";
+
+import App from './App.js'
+
+export default function Home() {
+    return (
+        <StrictMode>
+            <App />
+        </StrictMode>
+    )
+}
