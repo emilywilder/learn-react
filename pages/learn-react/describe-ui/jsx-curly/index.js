8b9dd84ba34c8dc9005695c7d02a21c5a7c13606
Emily Lockhart <validitymaiden@gmail.com>
2023-09-18 15:59:59 -0400

    add jsx-curly challenge 1

--- pages/learn-react/describe-ui/jsx-curly/index.js
+++ pages/learn-react/describe-ui/jsx-curly/index.js
@@ -2,9 +2,10 @@ import React, { StrictMode } from "react";
 import "./styles.css";
 // import "./styleExample.css"
 
-import App from './App'
+// import App from './App'
 // import App from './DateExample'
 // import App from './StyleExample'
+import App from './Challenge1'
 
 export default function Home() {
     return (
