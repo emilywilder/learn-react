1750e7ef781096a4f7fb18ed5c9bac04f80234ef
Emily Lockhart <validitymaiden@gmail.com>
2023-09-18 14:52:43 -0400

    add challenge for jsx-markup

--- pages/learn-react/describe-ui/jsx-markup/index.js
+++ pages/learn-react/describe-ui/jsx-markup/index.js
@@ -1,7 +1,9 @@
 import React, { StrictMode } from "react";
-import "./styles.css";
+// import "./styles.css";
+import "./challengeStyles.css"
 
-import App from './App.js'
+// import App from './App.js'
+import App from './Challenge.js'
 
 export default function Home() {
     return (
