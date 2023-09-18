c769cce7bb3a3aaeaf6f42c5f8350d86074c92d0
Emily Lockhart <validitymaiden@gmail.com>
2023-09-18 16:27:56 -0400

    add sending props in Profile to Avatar

--- pages/learn-react/describe-ui/props/App.jsx
+++ pages/learn-react/describe-ui/props/App.jsx
@@ -12,6 +12,9 @@ function Avatar() {
 
 export default function Profile() {
     return (
-        <Avatar />
+        <Avatar 
+            person={ { name: 'Lin Lanying', imageId: '1bX5QH6' } }
+            size={100}
+        />
     )
 }
