a948392ca866117d49ccec0846bba0cd9ff27775
Emily Lockhart <validitymaiden@gmail.com>
2023-09-18 14:43:47 -0400

    wrap the html in a fragment, returning a single element

--- pages/learn-react/describe-ui/jsx-markup/App.js
+++ pages/learn-react/describe-ui/jsx-markup/App.js
@@ -1,15 +1,17 @@
 export default function TodoList() {
     return (
-        <h1>Hedy Lamarr's Todos</h1>
-        <img 
-          src="https://i.imgur.com/yXOvdOSs.jpg" 
-          alt="Hedy Lamarr" 
-          class="photo"
-        >
-        <ul>
-            <li>Invent new traffic lights
-            <li>Rehearse a movie scene
-            <li>Improve the spectrum technology
-        </ul> 
+        <>
+            <h1>Hedy Lamarr's Todos</h1>
+            <img 
+                src="https://i.imgur.com/yXOvdOSs.jpg" 
+                alt="Hedy Lamarr" 
+                class="photo"
+            >
+            <ul>
+                <li>Invent new traffic lights
+                <li>Rehearse a movie scene
+                <li>Improve the spectrum technology
+            </ul>             
+        </>
     )
 }
