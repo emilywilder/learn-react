5e6764e7422fcdd1f27f053522764e4abc46b8d1
Emily Lockhart <validitymaiden@gmail.com>
2023-09-19 12:45:49 -0400

    move Avatar into separate file encapsulate Profile in a Card, demonstrating parent/child relationship when nested

--- pages/learn-react/describe-ui/props/index.js
+++ pages/learn-react/describe-ui/props/index.js
@@ -1,8 +1,8 @@
 import React, { StrictMode } from "react";
 import "./styles.css";
 
-// import App from './App'
-import App from './SpreadExample'
+import App from './App'
+// import App from './SpreadExample'
 
 export default function Home() {
     return (
