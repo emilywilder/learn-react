152265abfc4ac4415e11ee356c68a93178c3b631
Emily Lockhart <validitymaiden@gmail.com>
2023-09-23 15:36:00 -0400

    adding Passing Props challenge 2

--- pages/learn-react/describe-ui/props/index.js
+++ pages/learn-react/describe-ui/props/index.js
@@ -1,11 +1,12 @@
 import React, { StrictMode } from "react";
-// import "./styles.css";
-import "./Challenge1-styles.css";
+import "./styles.css";
+// import "./Challenge1-styles.css";
 
 // import App from './App'
 // import App from './SpreadExample'
 // import App from './ClockExample-App'
-import App from './Challenge1'
+// import App from './Challenge1'
+import App from './Challenge2'
 
 export default function Home() {
     return (
