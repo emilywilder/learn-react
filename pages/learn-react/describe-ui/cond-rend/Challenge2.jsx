869859905921fe4914aa32f0ba4e7ba5d7524e5b
Emily Lockhart <validitymaiden@gmail.com>
2023-09-23 17:18:23 -0400

    conditional rendering challenge 2 solution

--- pages/learn-react/describe-ui/cond-rend/Challenge2.jsx
+++ pages/learn-react/describe-ui/cond-rend/Challenge2.jsx
@@ -2,6 +2,11 @@ function Item({ name, importance }) {
     return (
       <li className="item">
         {name}
+        {importance > 0 && (
+            <i>
+                {` (Importance: ${importance})`}
+            </i>
+        )}
       </li>
     );
   }
