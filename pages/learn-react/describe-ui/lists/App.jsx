55c92316f7079af3ec4f0403ad64025a2cd6bc10
Emily Lockhart <validitymaiden@gmail.com>
2023-09-23 18:47:24 -0400

    add key from data fo li's

--- pages/learn-react/describe-ui/lists/App.jsx
+++ pages/learn-react/describe-ui/lists/App.jsx
@@ -4,7 +4,7 @@ import { getImageUrl } from './utils.jsx'
 export default function List () {
     const chemists = people.filter(person => person.profession === 'chemist')
     const listItems = chemists.map(person => 
-        <li>
+        <li key={person.id}>
             <img
                 src={getImageUrl(person)}
                 alt={person.name}
