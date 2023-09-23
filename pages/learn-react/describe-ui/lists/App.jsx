bdb1dba789ecf24ff5e163834dc392f9fb9d8474
Emily Lockhart <validitymaiden@gmail.com>
2023-09-23 18:15:54 -0400

    add more structure to people data

--- pages/learn-react/describe-ui/lists/App.jsx
+++ pages/learn-react/describe-ui/lists/App.jsx
@@ -1,13 +1,33 @@
 const people = [
-    'Creola Katherine Johnson: mathematician',
-    'Mario José Molina-Pasquel Henríquez: chemist',
-    'Mohammad Abdus Salam: physicist',
-    'Percy Lavon Julian: chemist',
-    'Subrahmanyan Chandrasekhar: astrophysicist'
+    {
+        id: 0,
+        name: 'Creola Katherine Johnson',
+        profession: 'mathematician',        
+    },
+    {
+        id: 1,
+        name: 'Mario José Molina-Pasquel Henríquez',
+        profession: 'chemist',        
+    },
+    {
+        id: 2,
+        name: 'Mohammad Abdus Salam',
+        profession: 'physicist',        
+    },
+    {
+        name: 'Percy Lavon Julian',
+        profession: 'chemist',        
+    },
+    {
+        name: 'Subrahmanyan Chandrasekhar',
+        profession: 'astrophysicist',        
+    },
 ]
 
 export default function List () {
-    const listItems = people.map(person => <li>{person}</li>)
+    const listItems = people.map(person => 
+        <li>{person.name}: {person.profession}</li>
+    )
     return (
         <ul>
             {listItems}
