4625c92805adeecb8d82ab30fc6c4a318a8b8a46
Emily Lockhart <validitymaiden@gmail.com>
2023-09-23 17:46:07 -0400

    move li items into a list, then use map to render each item

--- pages/learn-react/describe-ui/lists/App.jsx
+++ pages/learn-react/describe-ui/lists/App.jsx
@@ -1,13 +1,16 @@
-export default function App () {
+const people = [
+    'Creola Katherine Johnson: mathematician',
+    'Mario José Molina-Pasquel Henríquez: chemist',
+    'Mohammad Abdus Salam: physicist',
+    'Percy Lavon Julian: chemist',
+    'Subrahmanyan Chandrasekhar: astrophysicist'
+]
+
+export default function List () {
+    const listItems = people.map(person => <li>{person}</li>)
     return (
-        <>
-            <ul>
-                <li>Creola Katherine Johnson: mathematician</li>
-                <li>Mario José Molina-Pasquel Henríquez: chemist</li>
-                <li>Mohammad Abdus Salam: physicist</li>
-                <li>Percy Lavon Julian: chemist</li>
-                <li>Subrahmanyan Chandrasekhar: astrophysicist</li>
-            </ul>
-        </>   
+        <ul>
+            {listItems}
+        </ul>  
     )
 }
