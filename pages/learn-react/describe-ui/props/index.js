b5bb472f303bb1b324725802fe583b5e27823197
Emily Lockhart <validitymaiden@gmail.com>
2023-09-23 14:41:26 -0400

    Add Clock Example to demonstrate how props are dynamic

--- pages/learn-react/describe-ui/props/index.js
+++ pages/learn-react/describe-ui/props/index.js
@@ -1,8 +1,9 @@
 import React, { StrictMode } from "react";
 import "./styles.css";
 
-import App from './App'
+// import App from './App'
 // import App from './SpreadExample'
+import App from './ClockExample-App'
 
 export default function Home() {
     return (
