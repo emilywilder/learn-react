f08c94c4cac966e481e69794f61633451e2bc719
Emily Lockhart <validitymaiden@gmail.com>
2023-09-23 15:30:13 -0400

    initial solution to Passing Props challenge 1

--- pages/learn-react/describe-ui/props/Challenge1.jsx
+++ pages/learn-react/describe-ui/props/Challenge1.jsx
@@ -1,57 +1,80 @@
 import { getImageUrl } from './Challenge1-utils.jsx';
 
+function Profile({ person }) {
+    const listDetails = person.details.map((details, count) => {
+        return (
+            <li key={count}>
+                <b>{details.title} </b>
+                {details.description}
+            </li>
+        )
+    })
+    return (
+        <section className="profile">
+        <h2>{person.name}</h2>
+        <img
+          className="avatar"
+          src={getImageUrl(person.imageId)}
+          alt={person.name}
+          width={70}
+          height={70}
+        />
+        <ul>
+            {listDetails}
+        </ul>
+      </section>
+    )
+}
+
 export default function Gallery() {
+    // could be moved into a Person component
+    const people = [
+        {
+            name: "Maria Skłodowska-Curie",
+            imageId: "szV5sdG",
+            details: [
+                {
+                    title: "Profession:",
+                    description: "physicist and chemist"
+                },
+                {
+                    title: "Awards: 4",
+                    description: "(Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)"
+                },
+                {
+                    title: "Discovered:",
+                    description: "polonium (element)"
+                }
+            ]
+        },
+        {
+            name: "Katsuko Saruhashi",
+            imageId: "YfeOqp2",
+            details: [
+                {
+                    title: "Profession:",
+                    description: "geochemist"
+                },
+                {
+                    title: "Awards: 2",
+                    description: "(Miyake Prize for geochemistry, Tanaka Prize)"
+                },
+                {
+                    title: "Discovered:",
+                    description: "a method for measuring carbon dioxide in seawater"
+                }
+            ]
+        }
+    ]
+    const listPeople = people.map((person, count) => {
+        return (
+            <Profile person={person} key={count}/>
+        )}
+    )
   return (
     <div>
-      <h1>Notable Scientists</h1>
-      <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src={getImageUrl('szV5sdG')}
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b> 
-            physicist and chemist
-          </li>
-          <li>
-            <b>Awards: 4 </b> 
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (element)
-          </li>
-        </ul>
-      </section>
-      <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src={getImageUrl('YfeOqp2')}
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b> 
-            geochemist
-          </li>
-          <li>
-            <b>Awards: 2 </b> 
-            (Miyake Prize for geochemistry, Tanaka Prize)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            a method for measuring carbon dioxide in seawater
-          </li>
-        </ul>
-      </section>
+        <h1>Notable Scientists</h1>
+        {listPeople}
     </div>
   );
 }
