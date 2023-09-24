5ec7898c1db9eadd1e2e45cae4501cc0a0e3db7b
Emily Lockhart <validitymaiden@gmail.com>
2023-09-24 09:51:41 -0400

    add keeping components pure challenge 1

--- pages/learn-react/describe-ui/purity/index.js
+++ pages/learn-react/describe-ui/purity/index.js
@@ -1,7 +1,9 @@
 import React, { StrictMode } from "react";
-import "./styles.css";
+// import "./styles.css";
+import "./Challenge1-styles.css";
 
-import App from './App'
+// import App from './App'
+import App from './Challenge1' // Challenge is in Challenge1-Clock.jsx
 
 export default function Home() {
     return (
