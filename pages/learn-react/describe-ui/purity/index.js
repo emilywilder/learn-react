b7dbb72d57837fafc7e97099b901fa965620b61c
Emily Lockhart <validitymaiden@gmail.com>
2023-09-24 11:28:04 -0400

    add keeping components pure challenge 3

--- pages/learn-react/describe-ui/purity/index.js
+++ pages/learn-react/describe-ui/purity/index.js
@@ -1,11 +1,13 @@
 import React, { StrictMode } from "react";
 // import "./styles.css";
 // import "./Challenge1-styles.css";
-import "./Challenge2-styles.css";
+// import "./Challenge2-styles.css";
+import "./Challenge3-styles.css";
 
 // import App from './App'
 // import App from './Challenge1' // Challenge is in Challenge1-Clock.jsx
-import App from './Challenge2'
+// import App from './Challenge2'
+import App from './Challenge3' // Challenge is in Challenge3-StoryTray.jsx
 
 export default function Home() {
     return (
