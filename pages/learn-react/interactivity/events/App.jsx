c5427a06d4d1755b1ded3b3004e3c24e88d7473e
Emily Lockhart <validitymaiden@gmail.com>
2023-09-24 12:07:11 -0400

    demonstrate defininig the callback within the button element

--- pages/learn-react/interactivity/events/App.jsx
+++ pages/learn-react/interactivity/events/App.jsx
@@ -1,10 +1,8 @@
 export default function Button() {
-    function handleClick() {
-        alert('You clicked me!')
-    }
-
     return (
-      <button onClick={handleClick}>
+      <button onClick={function handleClick() {
+        alert('You clicked me!')
+      }}>
         Click me
       </button>
     );
