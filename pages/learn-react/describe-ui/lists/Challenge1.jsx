9829e2d04bce390fc872c851d6ad9bdd50d8edea
Emily Lockhart <validitymaiden@gmail.com>
2023-09-23 20:14:17 -0400

    Rendering Lists challenge 1 solution

--- pages/learn-react/describe-ui/lists/Challenge1.jsx
+++ pages/learn-react/describe-ui/lists/Challenge1.jsx
@@ -1,24 +1,38 @@
 import { people } from './data.js';
 import { getImageUrl } from './utils.jsx';
 
+function isChemist(person) {
+    return person.profession === 'chemist'
+}
+
+function ListItems({ people }) {
+    const listItems = people.map(person =>
+        <li key={person.id}>
+        <img
+            src={getImageUrl(person)}
+            alt={person.name}
+        />
+        <p>
+            <b>{person.name}:</b>
+            {' ' + person.profession + ' '}
+            known for {person.accomplishment}
+        </p>
+        </li>
+    )
+    return (
+        <ul>
+          {listItems}
+        </ul>
+      )
+}
+
 export default function List() {
-  const listItems = people.map(person =>
-    <li key={person.id}>
-      <img
-        src={getImageUrl(person)}
-        alt={person.name}
-      />
-      <p>
-        <b>{person.name}:</b>
-        {' ' + person.profession + ' '}
-        known for {person.accomplishment}
-      </p>
-    </li>
-  );
   return (
     <article>
-      <h1>Scientists</h1>
-      <ul>{listItems}</ul>
+      <h1>Chemists</h1>
+      <ListItems people={people.filter(person => isChemist(person))} />
+      <h1>Everyone Else</h1>
+      <ListItems people={people.filter(person => !isChemist(person))} />
     </article>
-  );
+  )
 }
