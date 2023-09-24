468340259cc1a4d00fa3ba880a65b0d7406e1b1b
Emily Lockhart <validitymaiden@gmail.com>
2023-09-24 12:15:31 -0400

    movie button example

--- pages/learn-react/interactivity/events/App.jsx
+++ pages/learn-react/interactivity/events/App.jsx
@@ -1,10 +1,36 @@
-export default function Button() {
+function Button({ onClick, children }) {
     return (
-      <button onClick={() => {
-        alert('You clicked me!')
-      }}>
-        Click me
-      </button>
-    );
-  }
-  
+        <button onClick={onClick}>
+            {children}
+        </button>
+    )
+}
+
+function PlayButton({ movieName }) {
+    function handlePlayClick() {
+        alert(`Playing ${movieName}!`)
+    }
+
+    return (
+        <Button onClick={handlePlayClick}>
+            Play "{movieName}"
+        </Button>
+    )
+}
+
+function UploadButton() {
+    return (
+        <Button onClick={() => alert('Uploading!')}>
+            Upload Image
+        </Button>
+    )
+}
+
+export default function Toolbar() {
+    return (
+        <div>
+            <PlayButton movieName="Kiki's Delivery Service" />
+            <UploadButton />
+        </div>
+    )
+}
