063cfe69d3e3ef0dff90e121a9f6085ce685be27
Emily Lockhart <validitymaiden@gmail.com>
2023-09-23 20:53:27 -0400

    rendering lists challenge 2 solution

--- pages/learn-react/describe-ui/lists/Challenge2.jsx
+++ pages/learn-react/describe-ui/lists/Challenge2.jsx
@@ -1,9 +1,29 @@
 import { recipes } from './Challenge2-data.js';
 
+function Recipe({ recipe }) {
+    const listIngredients = recipe.ingredients.map((ingredient =>
+        <li key={ingredient}>
+            {ingredient}
+        </li>
+    ))
+    return (
+        <div>
+            <h2>{recipe.name}</h2>
+            <ul>
+                {listIngredients}
+            </ul>
+        </div>
+    )
+}
+
 export default function RecipeList() {
+    const listRecipes = recipes.map((recipe =>
+        <Recipe recipe={recipe} key={recipe.id}/>
+    ))
   return (
     <div>
       <h1>Recipes</h1>
+      {listRecipes}
     </div>
   );
 }
