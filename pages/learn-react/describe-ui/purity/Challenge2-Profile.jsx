b0ac13712e05f49ceb2570705c912eaa39680a2b
Emily Lockhart <validitymaiden@gmail.com>
2023-09-24 11:18:53 -0400

    add keeping components pure challenge 2

---
+++ pages/learn-react/describe-ui/purity/Challenge2-Profile.jsx
@@ -0,0 +1,30 @@
+import Panel from './Challenge2-Panel.jsx';
+import { getImageUrl } from './Challenge2-utils.js';
+
+let currentPerson;
+
+export default function Profile({ person }) {
+  currentPerson = person;
+  return (
+    <Panel>
+      <Header />
+      <Avatar />
+    </Panel>
+  )
+}
+
+function Header() {
+  return <h1>{currentPerson.name}</h1>;
+}
+
+function Avatar() {
+  return (
+    <img
+      className="avatar"
+      src={getImageUrl(currentPerson)}
+      alt={currentPerson.name}
+      width={50}
+      height={50}
+    />
+  );
+}
