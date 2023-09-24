f606a7e49280f774e688eb84216bb49a0a82aba4
Emily Lockhart <validitymaiden@gmail.com>
2023-09-24 13:45:30 -0400

    add events challenge 1 solution

--- pages/learn-react/interactivity/events/Challenge1.jsx
+++ pages/learn-react/interactivity/events/Challenge1.jsx
@@ -9,7 +9,7 @@ export default function LightSwitch() {
     }
   
     return (
-      <button onClick={handleClick()}>
+      <button onClick={handleClick}>
         Toggle the lights
       </button>
     );
