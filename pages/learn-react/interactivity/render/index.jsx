232ef25f6d6a54ba9d7bcf52e3e470559f548209
Emily Lockhart <validitymaiden@gmail.com>
2023-09-26 13:13:42 -0400

    Clock example, demonstrating how react only updates the dom nodes that have changed

--- pages/learn-react/interactivity/render/index.jsx
+++ pages/learn-react/interactivity/render/index.jsx
@@ -1,16 +1,11 @@
-import Gallery from './Gallery'
-import { createRoot } from 'react-dom/client'
-import { useEffect } from 'react'
+import React, { StrictMode } from "react";
 
-// NextJS requires an exported default function, and
-// have to use useEffect to ensure the content is not
-// rendered server side, where document is undefined
-// NextJS also uses __next as the root element id
+import App from './App'
+
 export default function Home() {
-    useEffect(() => {
-        if (document) {
-            const root = createRoot(document.getElementById('__next'))
-            root.render(<Gallery />)
-        }
-      }, []);
+    return (
+        <StrictMode>
+            <App />
+        </StrictMode>
+    )
 }
