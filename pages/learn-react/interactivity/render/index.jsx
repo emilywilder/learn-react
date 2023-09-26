91d9993635b8c72541e56880e829feb7b785d31d
Emily Lockhart <validitymaiden@gmail.com>
2023-09-26 12:58:37 -0400

    change the way the root is created and rendered to align with NextJS

--- pages/learn-react/interactivity/render/index.jsx
+++ pages/learn-react/interactivity/render/index.jsx
@@ -1,5 +1,16 @@
 import Image from './Image'
 import { createRoot } from 'react-dom/client'
+import { useEffect } from 'react'
 
-const root = createRoot(document.getElementById('root'))
-root.render(<Image />)
+// NextJS requires an exported default function, and
+// have to use useEffect to ensure the content is not
+// rendered server side, where document is undefined
+// NextJS also uses __next as the root element id
+export default function Home() {
+    useEffect(() => {
+        if (document) {
+            const root = createRoot(document.getElementById('__next'))
+            root.render(<Image />)
+        }
+      }, []);
+}
