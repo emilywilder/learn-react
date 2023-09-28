376a5133f7524267e1ae699c9019feec2b38377c
Emily Lockhart <validitymaiden@gmail.com>
2023-09-28 11:21:14 -0400

    add snapshot challenge

--- pages/learn-react/interactivity/snapshot/index.jsx
+++ pages/learn-react/interactivity/snapshot/index.jsx
@@ -1,7 +1,9 @@
 import React, { StrictMode } from "react";
-import "./styles.css";
+// import "./styles.css";
+import "./Challenge-styles.css";
 
-import App from './App'
+// import App from './App'
+import App from './Challenge'
 
 export default function Home() {
     return (
