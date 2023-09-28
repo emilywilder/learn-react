036c1bffc3527ce176a40334667318c703581e5d
Emily Lockhart <validitymaiden@gmail.com>
2023-09-28 11:19:08 -0400

    further demonstrate how state variables are aligned to renders. the current render is not definitive of the state variables from a past render.

--- pages/learn-react/interactivity/snapshot/App.jsx
+++ pages/learn-react/interactivity/snapshot/App.jsx
@@ -1,17 +1,33 @@
 import { useState } from 'react';
 
-export default function Counter() {
-  const [number, setNumber] = useState(0);
+export default function Form() {
+  const [to, setTo] = useState('Alice')
+  const [message, setMessage] = useState('Hello');
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    setTimeout(() => {
+      alert(`You said ${message} to ${to}`)
+    }, 5000)
+  }
 
   return (
-    <>
-      <h1>{number}</h1>
-      <button onClick={() => {
-        setNumber(number + 5);
-        setTimeout(() => {
-          alert(number);
-        }, 3000);
-      }}>+5</button>
-    </>
+    <form onSubmit={handleSubmit}>
+      <label>
+        To:{' '}
+        <select
+          value={to}
+          onChange={e => setTo(e.target.value)}>
+          <option value={"Alice"}>Alice</option>
+          <option value={"Bob"}>Bob</option>
+        </select>
+      </label>
+      <textarea
+        placeholder='Message'
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+      />
+      <button type='submit'>Send</button>
+    </form>
   )
 }
