23046c4164df616bd05f0ec47a2a0b0b92db158b
Emily Lockhart <validitymaiden@gmail.com>
2023-09-28 19:18:51 -0400

    add change-objects challenge 3

--- pages/learn-react/interactivity/update-objects/index.jsx
+++ pages/learn-react/interactivity/update-objects/index.jsx
@@ -1,11 +1,13 @@
 import React, { StrictMode } from "react"
 // import "./styles.css"
 // import "./Challenge1-styles.css"
-import "./Challenge2-styles.css"
+// import "./Challenge2-styles.css"
+import "./Challenge3-styles.css"
 
 // import App from './App'
 // import App from './Challenge1'
-import App from './Challenge2'
+// import App from './Challenge2'
+import App from './Challenge3'
 
 export default function Home() {
     return (
