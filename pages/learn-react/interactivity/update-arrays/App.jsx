bc80771fea3f6ff3232634f8a8168330dfac70ff
Emily Lockhart <validitymaiden@gmail.com>
2023-09-29 12:44:32 -0400

    using immer to allow for mutating calls

--- pages/learn-react/interactivity/update-arrays/App.jsx
+++ pages/learn-react/interactivity/update-arrays/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useImmer } from "use-immer";
 
 let nextId = 3
 const initialList = [
@@ -8,27 +9,21 @@ const initialList = [
 ]
 
 export default function BucketList() {
-  const [myList, setMyList] = useState(initialList)
-  const [yourList, setYourList] = useState(initialList)
+  const [myList, updateMyList] = useImmer(initialList)
+  const [yourList, updateYourList] = useImmer(initialList)
 
   function handleToggleMyList(artworkId, nextSeen) {
-    setMyList(myList.map(artwork => {
-      if (artwork.id === artworkId) {
-        return { ...artwork, seen: nextSeen }
-      } else {
-        return artwork
-      }
-    }))
+    updateMyList(draft => {
+      const artwork = draft.find( a => a.id === artworkId )
+      artwork.seen = nextSeen
+    })
   }
 
   function handleToggleYourList(artworkId, nextSeen) {
-    setYourList(yourList.map(artwork => {
-      if (artwork.id === artworkId) {
-        return { ...artwork, seen: nextSeen }
-      } else {
-        return artwork
-      }
-    }))
+    updateYourList(draft => {
+      const artwork = draft.find( a => a.id === artworkId )
+      artwork.seen = nextSeen
+    })
   }
 
   return (
