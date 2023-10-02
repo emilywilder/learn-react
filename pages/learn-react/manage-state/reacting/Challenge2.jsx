25ab22fcc83a5d13c8f3aee1a2b8fefe93a7e6ff
Emily Lockhart <validitymaiden@gmail.com>
2023-10-02 16:33:52 -0400

    pull in changes made to allow for a lesson without react moved loading js script and index.html into a lesson component, imported in index.jsx
    add reacting challenge 2

---
+++ pages/learn-react/manage-state/reacting/Challenge2.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import { createRoot } from 'react-dom/client'
+import Script from 'next/script';
+
+import index_html from './public/index.html'
+
+export default function App() {
+    useEffect(() =>  {
+      if (document) {
+          const root = createRoot(document.getElementById('__next'))
+          root.render(
+              <>
+                  <Script src="/no-react-lesson.js" />
+                  <div dangerouslySetInnerHTML={{__html: index_html}} />
+              </>   
+          )
+      }
+    }, []) 
+  }
