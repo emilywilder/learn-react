7b2a027a112502c6d00195038d0b0f3e505e7dfe
Emily Lockhart <validitymaiden@gmail.com>
2023-10-02 17:24:51 -0400

    add reacting challenge 2 solution

--- pages/learn-react/manage-state/reacting/Challenge2.jsx
+++ pages/learn-react/manage-state/reacting/Challenge2.jsx
@@ -1,20 +1,50 @@
+import { useState } from "react";
+
 export default function EditProfile() {
+    // toggles between viewing mode and editing mode
+    const [viewMode, setViewMode] = useState(true)
+    const [firstName, setFirstName] = useState('Jane')
+    const [lastName, setLastName] = useState('Jacobs')
+
+    function handleFirstNameChange(e) {
+        setFirstName(e.target.value)
+    }
+
+    function handleLastNameChange(e) {
+        setLastName(e.target.value)
+    }
+
+    function handleClick(e) {
+        e.preventDefault()
+        setViewMode(!viewMode)
+    }
+
     return (
-      <form>
+      <form onSubmit={handleClick}>
         <label>
           First name:{' '}
-          <b>Jane</b>
-          <input />
+          {viewMode ? 
+            <b>{firstName}</b> :
+            <input 
+                value={firstName}
+                onChange={handleFirstNameChange}
+            />
+          }
         </label>
         <label>
           Last name:{' '}
-          <b>Jacobs</b>
-          <input />
+          {viewMode ?
+            <b>{lastName}</b> :
+            <input 
+                value={lastName}
+                onChange={handleLastNameChange}
+            />
+          }
         </label>
         <button type="submit">
-          Edit Profile
+          {viewMode ? "Edit" : "Save"} Profile
         </button>
-        <p><i>Hello, Jane Jacobs!</i></p>
+        <p><i>Hello, {firstName} {lastName}!</i></p>
       </form>
     );
   }
