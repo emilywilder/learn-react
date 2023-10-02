3825a2220333056caaf80e29de33543d44e41a62
Emily Lockhart <validitymaiden@gmail.com>
2023-10-02 19:35:39 -0400

    add Choosing the State Structure lesson with initial example demonstrate an ideal situation where a single state variable for both x and y values makes more sense than separate state variables

---
+++ pages/learn-react/manage-state/state-structure/index.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react"
+
+import "./styles.css"
+
+import App from './App'
+
+export default function Home() {
+    return (
+        <StrictMode>
+            <App />
+        </StrictMode>
+    )
+}
