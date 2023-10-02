fc56a71178214a59304d7fccb50df6b134657cfb
Emily Lockhart <validitymaiden@gmail.com>
2023-10-02 19:11:06 -0400

    add reacting challenge 3 solution

--- public/manage-state-reacting-challenge3.js
+++ public/manage-state-reacting-challenge3.js
@@ -1,54 +1,69 @@
+let viewMode = true
+let firstName = 'Jane'
+let lastName = 'Jacobs'
+
+function setViewMode(value) {
+    viewMode = value
+}
+
+function setFirstName(name) {
+    firstName = name
+    updateDOM()
+}
+
+function setLastName(name) {
+    lastName = name
+    updateDOM()
+}
+    
+function handleFirstNameChange(e) {
+    setFirstName(e.target.value)
+}
+  
+function handleLastNameChange(e) {
+    setLastName(e.target.value)
+}
+
 function handleFormSubmit(e) {
     e.preventDefault();
-    if (editButton.textContent === 'Edit Profile') {
-      editButton.textContent = 'Save Profile';
-      hide(firstNameText);
-      hide(lastNameText);
-      show(firstNameInput);
-      show(lastNameInput);
+    setViewMode(!viewMode)
+    updateDOM()
+}
+
+function updateDOM() {
+    firstNameText.textContent = firstName
+    lastNameText.textContent = lastName
+    helloText.textContent = `Hello, ${firstName} ${lastName}!`
+    if (viewMode) {
+        editButton.textContent = 'Edit Profile'
+        hide(firstNameInput);
+        hide(lastNameInput);
+        show(firstNameText);
+        show(lastNameText);
     } else {
-      editButton.textContent = 'Edit Profile';
-      hide(firstNameInput);
-      hide(lastNameInput);
-      show(firstNameText);
-      show(lastNameText);
+        editButton.textContent = 'Save Profile'
+        hide(firstNameText);
+        hide(lastNameText);
+        show(firstNameInput);
+        show(lastNameInput);
     }
-  }
-  
-  function handleFirstNameChange() {
-    firstNameText.textContent = firstNameInput.value;
-    helloText.textContent = (
-      'Hello ' +
-      firstNameInput.value + ' ' +
-      lastNameInput.value + '!'
-    );
-  }
-  
-  function handleLastNameChange() {
-    lastNameText.textContent = lastNameInput.value;
-    helloText.textContent = (
-      'Hello ' +
-      firstNameInput.value + ' ' +
-      lastNameInput.value + '!'
-    );
-  }
-  
-  function hide(el) {
+}
+
+function hide(el) {
     el.style.display = 'none';
-  }
+}
   
-  function show(el) {
+function show(el) {
     el.style.display = '';
-  }
-  
-  let form = document.getElementById('form');
-  let editButton = document.getElementById('editButton');
-  let firstNameInput = document.getElementById('firstNameInput');
-  let firstNameText = document.getElementById('firstNameText');
-  let lastNameInput = document.getElementById('lastNameInput');
-  let lastNameText = document.getElementById('lastNameText');
-  let helloText = document.getElementById('helloText');
-  form.onsubmit = handleFormSubmit;
-  firstNameInput.oninput = handleFirstNameChange;
-  lastNameInput.oninput = handleLastNameChange;
-  
+}
+
+let form = document.getElementById('form');
+let editButton = document.getElementById('editButton');
+let firstNameInput = document.getElementById('firstNameInput');
+let firstNameText = document.getElementById('firstNameText');
+let lastNameInput = document.getElementById('lastNameInput');
+let lastNameText = document.getElementById('lastNameText');
+let helloText = document.getElementById('helloText');
+form.onsubmit = handleFormSubmit;
+firstNameInput.oninput = handleFirstNameChange;
+lastNameInput.oninput = handleLastNameChange;
