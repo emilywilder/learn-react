86b96e889d2782ad22845a75047e7525e765e15b
Emily Lockhart <validitymaiden@gmail.com>
2023-10-02 16:06:13 -0400

    add reacting challenge 1 solution

--- pages/learn-react/manage-state/reacting/Challenge1.jsx
+++ pages/learn-react/manage-state/reacting/Challenge1.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+
 export default function Picture() {
+    const [backgroundIsActive, setBackgroundIsActive] = useState(true)
+    
     return (
-      <div className="background background--active">
+      <div className={`background ${backgroundIsActive ? 'background--active' : ''}`}
+        onClick={() => setBackgroundIsActive(true)}>
         <img
-          className="picture"
+          className={`picture ${backgroundIsActive ? '' : 'picture--active'}`}
           alt="Rainbow houses in Kampung Pelangi, Indonesia"
           src="https://i.imgur.com/5qwVYb1.jpeg"
+          onClick={(e) => {
+            e.stopPropagation()
+            setBackgroundIsActive(false)
+          }}
         />
       </div>
     );
