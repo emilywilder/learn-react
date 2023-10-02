b605f07350a76b2a80daef6ffdfff52aa6e4861e
Emily Lockhart <validitymaiden@gmail.com>
2023-10-02 15:43:25 -0400

    add reacting challenge 1

--- pages/learn-react/manage-state/reacting/index.jsx
+++ pages/learn-react/manage-state/reacting/index.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react"
 
-import "./styles.css"
-import App from './App'
+// import "./styles.css"
+import "./Challenge1-styles.css"
+// import App from './App'
+import App from './Challenge1'
 
 export default function Home() {
     return (
