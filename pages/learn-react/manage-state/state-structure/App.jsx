abd194817e50f90e4887cca145f6386ceb33a5e8
Emily Lockhart <validitymaiden@gmail.com>
2023-10-03 11:23:10 -0400

    demonstrate adding buttons to remove places with normalized data

--- pages/learn-react/manage-state/state-structure/App.jsx
+++ pages/learn-react/manage-state/state-structure/App.jsx
@@ -1,25 +1,25 @@
 import { useState } from 'react';
 import { initialTravelPlan } from './places';
 
-function PlaceTree({ id, placesById }) {
-  const place = placesById[id]
-  const childIds = place.childIds;
-  return (
-    <li>
-      {place.title}
-      {childIds.length > 0 && (
-        <ol>
-          {childIds.map(childId => (
-            <PlaceTree key={childId} id={childId} placesById={placesById} />
-          ))}
-        </ol>
-      )}
-    </li>
-  );
-}
-
 export default function TravelPlan() {
   const [plan, setPlan] = useState(initialTravelPlan);
+
+  function handleComplete(parentId, childId) {
+    const parent = plan[parentId]
+    // remove place from parent's list of children ids
+    const nextParent = {
+        ...parent,
+        childIds: parent.childIds.filter(
+            id => id !== childId
+        )
+    }
+    // update plan with new parent object
+    setPlan({
+        ...plan,
+        [parentId]: nextParent
+    })
+  }
+
   const root = plan[0]
   const planetIds = root.childIds
   return (
@@ -27,9 +27,41 @@ export default function TravelPlan() {
       <h2>Places to visit</h2>
       <ol>
         {planetIds.map(id => (
-          <PlaceTree key={id} id={id} placesById={plan} />
+          <PlaceTree
+              key={id}
+              id={id}
+              parentId={0}
+              placesById={plan}
+              onComplete={handleComplete}
+          />
         ))}
       </ol>
     </>
   );
 }
+
+function PlaceTree({ id, parentId, placesById, onComplete }) {
+    const place = placesById[id]
+    const childIds = place.childIds;
+    return (
+      <li>
+        {place.title}
+        <button onClick={() => {onComplete(parentId, id)}}>
+            Complete
+        </button>
+        {childIds.length > 0 && (
+          <ol>
+            {childIds.map(childId => (
+              <PlaceTree
+                key={childId}
+                id={childId}
+                parentId={id}
+                placesById={placesById}
+                onComplete={onComplete}
+              />
+            ))}
+          </ol>
+        )}
+      </li>
+    );
+  }  
