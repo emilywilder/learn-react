af4d05216a733a11b1c5d14b197f5a7b668abe7f
Emily Lockhart <validitymaiden@gmail.com>
2023-10-03 10:25:54 -0400

    demonstrate a situation where two state variables could be set such that it would introduce an 'impossible' state for the logic of the page.

--- pages/learn-react/manage-state/state-structure/App.jsx
+++ pages/learn-react/manage-state/state-structure/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-export default function MovingDot() {
-    const [position, setPosition] = useState({
-        x: 0,
-        y: 0
-    })
-    return (
-        <div
-            onPointerMove={e => {
-                setPosition({
-                    x: e.clientX,
-                    y: e.clientY
-                })
-            }}
-            style={{
-                position: 'relative',
-                width: '100vw',
-                height: '100vh'
-            }}>
-            <div
-                style={{
-                    position: 'absolute',
-                    backgroundColor: 'red',
-                    borderRadius: '50%',
-                    transform: `translate(${position.x}px, ${position.y}px)`,
-                    left: -10,
-                    top: -10,
-                    width: 20,
-                    height: 20
-                }}
-            />
-        </div>
-    )
+export default function FeedbackForm() {
+  const [text, setText] = useState('');
+  const [isSending, setIsSending] = useState(false);
+  const [isSent, setIsSent] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setIsSending(true);
+    await sendMessage(text);
+    setIsSending(false);
+    setIsSent(true);
+  }
+
+  if (isSent) {
+    return <h1>Thanks for feedback!</h1>
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>How was your stay at The Prancing Pony?</p>
+      <textarea
+        disabled={isSending}
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+      <br />
+      <button
+        disabled={isSending}
+        type="submit"
+      >
+        Send
+      </button>
+      {isSending && <p>Sending...</p>}
+    </form>
+  );
+}
+
+// Pretend to send a message.
+function sendMessage(text) {
+  return new Promise(resolve => {
+    setTimeout(resolve, 2000);
+  });
 }
