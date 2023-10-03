abe56a74beaa0eea2a91adb28564b047f12abfb8
Emily Lockhart <validitymaiden@gmail.com>
2023-10-03 10:48:57 -0400

    demonstrate how storing the id of the selected item in state, rather than the item itself, allows for the item to be remembered as selected and also avoids duplication in state.

--- pages/learn-react/manage-state/state-structure/App.jsx
+++ pages/learn-react/manage-state/state-structure/App.jsx
@@ -8,9 +8,11 @@ const initialItems = [
 
 export default function Menu() {
   const [items, setItems] = useState(initialItems);
-  const [selectedItem, setSelectedItem] = useState(
-    items[0]
-  );
+  const [selectedId, setSelectedId] = useState(0);
+
+  const selectedItem = items.find(item =>
+    item.id === selectedId
+  )
 
   function handleItemChange(id, e) {
     setItems(items.map(item => {
@@ -39,7 +41,7 @@ export default function Menu() {
           />
             {' '}
             <button onClick={() => {
-              setSelectedItem(item);
+              setSelectedId(item.id);
             }}>Choose</button>
           </li>
         ))}
