1b95e880dd7836a5cf222747a7212ac3715ab4c5
Emily Lockhart <validitymaiden@gmail.com>
2023-10-04 11:44:40 -0400

    add state-structure challenge 1

--- pages/learn-react/manage-state/state-structure/index.jsx
+++ pages/learn-react/manage-state/state-structure/index.jsx
@@ -1,13 +1,15 @@
-import { StrictMode } from "react"
+import { StrictMode, useEffect } from "react"
 
-import "./styles.css"
+// import "./styles.css"
+import "./Challenge1-styles.css"
 
-import App from './App'
+// import App from './App'
+import App from './Challenge1' // challenge in Challenge1-Clock.jsx
 
 export default function Home() {
     return (
         <StrictMode>
             <App />
         </StrictMode>
-    )
+    )    
 }
