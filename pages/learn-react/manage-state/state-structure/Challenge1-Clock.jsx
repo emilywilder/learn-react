e5e9f28776b1a7e9197f67d323e68ecfd6ca375d
Emily Lockhart <validitymaiden@gmail.com>
2023-10-04 11:46:08 -0400

    add state-structure challenge 1 solution color is passed through props, so no need to use a state variable

--- pages/learn-react/manage-state/state-structure/Challenge1-Clock.jsx
+++ pages/learn-react/manage-state/state-structure/Challenge1-Clock.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react';
 
 export default function Clock(props) {
-  const [color, setColor] = useState(props.color);
   return (
-    <h1 style={{ color: color }}>
+    <h1 style={{ color: props.color }}>
       {props.time}
     </h1>
   );
