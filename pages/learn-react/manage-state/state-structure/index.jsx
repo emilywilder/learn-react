5a654970ea7565d9660ca7b1510637b6b8a203c1
Emily Lockhart <validitymaiden@gmail.com>
2023-10-05 00:22:29 -0400

    add state-structure challenge 4

--- pages/learn-react/manage-state/state-structure/index.jsx
+++ pages/learn-react/manage-state/state-structure/index.jsx
@@ -3,12 +3,14 @@ import { StrictMode } from "react"
 // import "./styles.css"
 // import "./Challenge1-styles.css"
 // import "./Challenge2-styles.css"
-import "./Challenge3-styles.css"
+// import "./Challenge3-styles.css"
+import "./Challenge4-styles.css"
 
 // import App from './App'
 // import App from './Challenge1' // challenge in Challenge1-Clock.jsx
 // import App from './Challenge2'
-import App from './Challenge3'
+// import App from './Challenge3'
+import App from './Challenge4'
 
 export default function Home() {
     return (
