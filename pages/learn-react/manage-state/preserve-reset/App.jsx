c587f9f35421ddd9905a861a150de469055fbbcc
Emily Lockhart <validitymaiden@gmail.com>
2023-10-07 20:36:55 -0400

    demonstrate that react maintains a components state as long as it is being rendered at its position.

--- pages/learn-react/manage-state/preserve-reset/App.jsx
+++ pages/learn-react/manage-state/preserve-reset/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from "react";
 
 export default function App() {
+    const [showB, setShowB] = useState(true)
     return (
         <div>
             <Counter />
-            <Counter />
+            {showB && <Counter />}
+            <label>
+                <input
+                    type="checkbox"
+                    checked={showB}
+                    onChange={e => {
+                        setShowB(e.target.checked)
+                    }}
+                />
+                Render the second counter
+            </label>
         </div>
     )
 }
