767f72bd9fe47a793b8bb3b59b70897b0e92ed0c
Emily Lockhart <validitymaiden@gmail.com>
2023-10-07 22:35:20 -0400

    add preserve-reset challenge 3

--- pages/learn-react/manage-state/preserve-reset/index.jsx
+++ pages/learn-react/manage-state/preserve-reset/index.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from "react"
 
 // import "./styles.css"
 // import "./Challenge1-styles.css"
-import "./Challenge2.css"
+// import "./Challenge2.css"
+import "./Challenge3-styles.css"
 
 // import App from './App'
 // import App from './NestedDefinitionExample'
-import App from './Challenge2'
+// import App from './Challenge2'
+import App from './Challenge3-App'
 
 function Blank() {
     return (
