54dac5b78d6e3184292abb633a447ff779ccd6f3
Emily Lockhart <validitymaiden@gmail.com>
2023-10-11 18:28:04 -0400

    add reducer challenge 2

--- pages/learn-react/manage-state/reducer/index.jsx
+++ pages/learn-react/manage-state/reducer/index.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react"
 
 // import "./styles.css"
-import "./Challenge1-styles.css"
+// import "./Challenge1-styles.css"
+import "./Challenge2-styles.css"
 
 // import App from './App'
 // import App from './ReducerExample'
-import App from './Challenge1-App'
+// import App from './Challenge1-App'
+import App from './Challenge2-App'
 
 function Blank() {
     return (
