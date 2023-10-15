11b8a53fea8115f01c15f95701f3996af92f3afd
Emily Lockhart <validitymaiden@gmail.com>
2023-10-15 16:34:32 -0400

    add lesson Scaling Up with Reducer and Context

---
+++ pages/learn-react/manage-state/scaling/index.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react"
+
+// import "./styles.css"
+
+// import App from './App'
+
+function Blank() {
+    return (
+        <p>This app is yet to be defined.</p>
+    )
+}
+
+export default function Home() {
+    return (
+        <StrictMode>
+            <Blank />
+        </StrictMode>
+    )
+}
