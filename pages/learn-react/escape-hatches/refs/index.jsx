126f580503889de8473c63702cdbbbae5693ae79
Emily Lockhart <validitymaiden@gmail.com>
2023-10-16 13:05:23 -0400

    add refs challenge 2

--- pages/learn-react/escape-hatches/refs/index.jsx
+++ pages/learn-react/escape-hatches/refs/index.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react"
 
 // import "./styles.css"
-import "./Challenge1-styles.css"
+// import "./Challenge1-styles.css"
+import "./Challenge2-styles.css"
 
 // import App from './App'
-import App from './Challenge1-App'
+// import App from './Challenge1-App'
+import App from './Challenge2-App'
 
 function Blank() {
     return (
