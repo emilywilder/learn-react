be3501c61f33e6b60a235549c6624faf6530debe
Emily Lockhart <validitymaiden@gmail.com>
2023-10-16 17:59:43 -0400

    add refs-dom challenge 1 solution
    add a ref for the video dom object and use pause() or play() when the button is clicked

--- pages/learn-react/escape-hatches/refs-dom/Challenge1-App.jsx
+++ pages/learn-react/escape-hatches/refs-dom/Challenge1-App.jsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function VideoPlayer() {
     const [isPlaying, setIsPlaying] = useState(false)
+    const videoRef = useRef(null)
 
     function handleClick() {
         const nextIsPlaying = !isPlaying
         setIsPlaying(nextIsPlaying)
+        if (isPlaying) {
+            videoRef.current.pause()
+        } else {
+            videoRef.current.play()
+        }
+
     }
 
     return (
@@ -13,7 +20,7 @@ export default function VideoPlayer() {
             <button onClick={handleClick}>
                 {isPlaying ? 'Pause' : 'Play'}
             </button>
-            <video width="250">
+            <video ref={videoRef} width="250">
                 <source
                     src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
                     type="video/mp4"
