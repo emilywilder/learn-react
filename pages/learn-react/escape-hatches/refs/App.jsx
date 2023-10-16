157b5e47e097647a160d826319cbd4d0216e9999
Emily Lockhart <validitymaiden@gmail.com>
2023-10-16 12:00:39 -0400

    demonstrate that react re-renders when count uses state

--- pages/learn-react/escape-hatches/refs/App.jsx
+++ pages/learn-react/escape-hatches/refs/App.jsx
@@ -1,40 +1,15 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
-export default function Stopwatch() {
-    const [startTime, setStartTime] = useState(null)
-    const [now, setNow] = useState(null)
-    const intervalRef = useRef(null)
+export default function Counter() {
+    const [count, setCount] = useState(0)
 
-    function handleStart() {
-        // Start counting.
-        setStartTime(Date.now())
-        setNow(Date.now())
-
-        clearInterval(intervalRef.current)
-        intervalRef.current = setInterval(() => {
-            // Update the current time every 10ms.
-            setNow(Date.now())
-        }, 10)
-    }
-
-    function handleStop() {
-        clearInterval(intervalRef.current)
-    }
-
-    let secondsPassed = 0
-    if (startTime != null && now != null) {
-        secondsPassed = (now - startTime) / 1000
+    function handleClick() {
+        setCount(count + 1)
     }
 
     return (
-        <>
-            <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
-            <button onClick={handleStart}>
-                Start
-            </button>
-            <button onClick={handleStop}>
-                Stop
-            </button>
-        </>
+        <button onClick={handleClick}>
+            You clicked {count} times
+        </button>
     )
 }
