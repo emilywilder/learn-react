bef28eb53a80e2a7b37d93d48cc14c93c0964c98
Emily Lockhart <validitymaiden@gmail.com>
2023-10-18 11:56:37 -0400

    add refs-dom challenge 2 solution
    add a ref to the input dom object, and add an onClick event handler to focus that ref

--- pages/learn-react/escape-hatches/refs-dom/Challenge2-App.jsx
+++ pages/learn-react/escape-hatches/refs-dom/Challenge2-App.jsx
@@ -1,10 +1,18 @@
+import { useRef } from "react"
+
 export default function Page() {
+    const inputRef = useRef(null)
     return (
         <>
             <nav>
-                <button>Search</button>
+                <button onClick={() => {
+                    inputRef.current.focus()
+                }}>
+                    Search
+                </button>
             </nav>
             <input
+                ref={inputRef}
                 placeholder="Looking for something?"
             />
         </>
