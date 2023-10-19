28418b50b5505f59ad9e585c7ee83eb64462d12d
Emily Lockhart <validitymaiden@gmail.com>
2023-10-18 20:32:47 -0400

    add refs-dom challenge 4

---
+++ pages/learn-react/escape-hatches/refs-dom/Challenge4-App.jsx
@@ -0,0 +1,13 @@
+import SearchButton from "./Challenge4-SearchButton";
+import SearchInput from "./Challenge4-SearchInput";
+
+export default function Page() {
+    return (
+        <>
+            <nav>
+                <SearchButton />
+            </nav>
+            <SearchInput />
+        </>
+    )
+}
