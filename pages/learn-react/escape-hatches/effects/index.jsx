10a4d8b114c43b93ac45bfa0b79fbc97cffed710
Emily Lockhart <validitymaiden@gmail.com>
2023-10-19 12:12:00 -0400

    add effects challenge 4

--- pages/learn-react/escape-hatches/effects/index.jsx
+++ pages/learn-react/escape-hatches/effects/index.jsx
@@ -3,12 +3,14 @@ import { StrictMode } from "react"
 // import "./styles.css"
 // import "./Challenge1-styles.css"
 // import "./Challenge2-styles.css"
-import "./Challenge3-styles.css"
+// import "./Challenge3-styles.css"
+import "./Challenge4-styles.css"
 
 // import App from './App'
 // import App from './Challenge1-App'
 // import App from './Challenge2-App'
-import App from './Challenge3-App'
+// import App from './Challenge3-App'
+import App from './Challenge4-App'
 
 function Blank() {
     return (
