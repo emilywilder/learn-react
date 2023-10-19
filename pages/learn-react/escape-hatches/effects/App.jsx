b705a3a22e6e1af83ac4d4e325c7a130fb1a43de
Emily Lockhart <validitymaiden@gmail.com>
2023-10-19 11:18:58 -0400

    demonstrate cleanup functions for effects

--- pages/learn-react/escape-hatches/effects/App.jsx
+++ pages/learn-react/escape-hatches/effects/App.jsx
@@ -1,11 +1,46 @@
-import { useEffect } from "react";
-import createConnection from "./chat";
+import { useEffect, useState } from "react";
 
-export default function ChatRoom() {
+function Playground() {
+    const [text, setText] = useState('a')
+
     useEffect(() => {
-        const connection = createConnection()
-        connection.connect()
-        return () => connection.disconnect()
-    }, [])
-    return <h1>Welcome to the Chat!</h1>
+        function onTimeout() {
+            console.log('⏰ ' + text)
+        }
+
+        console.log('🔵 Schedule "' + text + '" log')
+        const timeoutId = setTimeout(onTimeout, 3000)
+
+        return () => {
+            console.log('🟡 Cancel "' + text + '" log')
+            clearTimeout(timeoutId)
+        }
+    }, [text])
+
+    return (
+        <>
+            <label>
+                What to log:{' '}
+                <input
+                    value={text}
+                    onChange={e => setText(e.target.value)}
+                />
+            </label>
+            <h1>{text}</h1>
+        </>
+    )
+}
+
+export default function App() {
+    const [show, setShow] = useState(false)
+
+    return (
+        <>
+            <button onClick={() => setShow(!show)}>
+                {show ? 'Unmount' : 'Mount' } the component
+            </button>
+            {show && <hr />}
+            {show && <Playground />}
+        </>
+    )
 }
