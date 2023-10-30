c0e345e34194b776e9bc54f129e118c3c1726321
Emily Lockhart <validitymaiden@gmail.com>
2023-10-29 20:08:41 -0400

    add tiles demonstrating better and worse ways to implement sharing logic between event handlers
    event handlers not yet implemented
    
    add background image to /public

--- pages/learn-react/escape-hatches/need-effect/App.jsx
+++ pages/learn-react/escape-hatches/need-effect/App.jsx
@@ -1,161 +1,93 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react";
 
-function BadList({ items }) {
-    const [isReverse, setIsReverse] = useState(false);
-    const [selection, setSelection] = useState(null);
-  
-    // 🔴 Avoid: Adjusting state on prop change in an Effect
+function BadProductPage({ product, addToCart }) {
+    // 🔴 Avoid: Event-specific logic inside an Effect
     useEffect(() => {
-      setSelection(null);
-    }, [items]);
-    // ...
-    const selectedId = selection ? selection.id : null
-    function handleChange(id) {
-        const selection = items.find((x) => x.id === id)
-        setSelection(selection)
-    }
-    return <ListItems items={items} selectedId={selectedId} onChange={handleChange}/>
-}
-
-function BetterList({ items }) {
-    const [isReverse, setIsReverse] = useState(false);
-    const [selection, setSelection] = useState(null);
+        if (product.isInCart) {
+            showNotification(`Added ${product.name} to the shopping cart!`);
+        }
+    }, [product]);
   
-    // Better: Adjust the state while rendering
-    const [prevItems, setPrevItems] = useState(items);
-    if (items !== prevItems) {
-      setPrevItems(items);
-      setSelection(null);
+    function handleBuyClick() {
+        addToCart(product);
+    }
+  
+    function handleCheckoutClick() {
+        addToCart(product);
+        navigateTo('/checkout');
     }
     // ...
-    const selectedId = selection ? selection.id : null
-    function handleChange(id) {
-        const selection = items.find((x) => x.id === id)
-        setSelection(selection)
-    }
-    return <ListItems items={items} selectedId={selectedId} onChange={handleChange}/>
+    return <RenderProduct
+                product={product}
+                onByClick={handleBuyClick}
+                onCheckoutClick={handleCheckoutClick}
+            />
 }
 
-function BestList({ items }) {
-    const [isReverse, setIsReverse] = useState(false);
-    const [selectedId, setSelectedId] = useState(null);
-    // ✅ Best: Calculate everything during rendering
-    const selection = items.find(item => item.id === selectedId) ?? null;
-    // ...
-    function handleChange(id) {
-        setSelectedId(id)
-    }
-    return <ListItems items={items} selectedId={selectedId} onChange={handleChange}/>
-}
-
-function ListItems({ items, selectedId, onChange }) {
+function Button({ children }) {
     return (
-        <form>
-            {items.map((x) => (
-                <label key={x.id}>
-                    <input
-                        className="sr-only peer"
-                        type="radio"
-                        name={x.item}
-                        value={x.item}
-                        checked={x.id === selectedId}
-                        onChange={() => onChange(x.id)}
-                    />
-                    <div className="rounded p-1 peer-checked:text-white peer-checked:bg-blue-400">
-                        {x.item}
-                    </div>
-                </label>
+        <div className="p-2 m-1 bg-blue-400 rounded text-white font-bold hover:bg-blue-500">
+            {children}
+        </div>
+    )
+}
 
-            ))}
-        </form>
+function RenderProduct({ product, onByClick, onCheckoutClick }) {
+    return (
+        <div className="flex flex-col w-64">
+            <div className="h-full w-full bg-contain rounded bg-[url('/chester-alvarez-bphc6kyobMg-unsplash.jpg')]">
+                <div className="h-full backdrop-brightness-50 rounded-lg">
+                    <div className="text-white text-3xl p-2">
+                        {product.name}
+                    </div>
+                    <div className="flex justify-end m-1">
+                        <Button onClick={onByClick}>
+                            Buy
+                        </Button>
+                        <Button onClick={onCheckoutClick}>
+                            Checkout
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function RenderProductPage({ ProductPage, header }) {
+    const product = {
+        id: 0,
+        name: 'Ultimate Fancy Gadget Mark II',
+        isInCart: false
+    }
+    function addToCart(product) {
+        product.isInCart = true
+    }
+
+    return (
+        <div className={`m-2 rounded outline outline-${header.color} bg-${header.color}`}>
+            <div className="p-2 text-white font-bold">
+                {header.text}
+            </div>
+            <ProductPage product={product} addToCart={addToCart} />
+        </div>
     )
 }
 
 export default function App() {
-    const [person, setPerson] = useState('')
-    const filteredItems = items.filter((x) => x.name === person)
-
+    const headers = {
+        'good': {
+            'text': "Good Page",
+            'color': 'green-400'
+        },
+        'bad': {
+            'text': "Bad Page",
+            'color': 'red-400'
+        }
+    }
     return (
-        <div className="m-3">
-            <form className="flex flex-wrap">
-                {getUniqueNames(items).map((name) => (
-                    <label key={name}>
-                    <input
-                        className="sr-only peer"
-                        type="radio"
-                        name={name}
-                        value={name}
-                        checked={name === person}
-                        onChange={e => setPerson(e.target.value)}
-                    />
-                    <div className="text-sm p-2 rounded-lg peer-checked:bg-blue-400 peer-checked:text-white">
-                        {name}
-                    </div>
-                </label>
-                ))}
-            </form>
-            { person ? (
-                <div className="p-2 flex flex-wrap">
-                    <div className="flex w-36 m-2 flex-col rounded-xl overflow-hidden bg-white bg-clip-border shadow-md">
-                        <div className="flex h-20 bg-red-400 text-white p-2">
-                            Adjust state after render
-                        </div>
-                        <div className="p-2 text-center">
-                            <BadList items={filteredItems} />
-                        </div>
-                    </div>
-                    <div className="flex w-36 m-2 flex-col rounded-xl overflow-hidden bg-white bg-clip-border shadow-md">
-                        <div className="flex h-20 bg-yellow-400 text-white p-2 justify-center">
-                            Adjust state during render
-                        </div>
-                        <div className="p-2 text-center">
-                            <BetterList items={filteredItems} />
-                        </div>
-                    </div>
-                    <div className="flex w-36 m-2 flex-col rounded-xl overflow-hidden bg-white bg-clip-border shadow-md">
-                        <div className="flex h-20 bg-green-400 text-white p-2">
-                            Calculate during render
-                        </div>
-                        <div className="p-2 text-center">
-                            <BestList items={filteredItems} />
-                        </div>
-                    </div>
-                </div>
-            ) : (
-                <div className="mt-3 ms-3">Select a person to see their list items.</div>
-            )}
+        <div className="flex flex-wrap m-3">
+            <RenderProductPage ProductPage={BadProductPage} header={headers.bad} />
         </div>
     )
 }
-function getUniqueNames(list) {
-    const names = list.map((x) => x.name)
-    return [...new Set(names)]
-}
-
-const items = [
-    {
-        id: 0,
-        name: 'Betty',
-        item: 'Oat Milk',
-    },
-    {
-        id: 1,
-        name: 'Veronica',
-        item: 'Bagels',
-    },
-    {
-        id: 2,
-        name: 'Betty',
-        item: 'Tofu',
-    },
-    {
-        id: 3,
-        name: 'Veronica',
-        item: 'Coffee',
-    },
-    {
-        id: 4,
-        name: 'Veronica',
-        item: 'Milk',
-    },
-]
