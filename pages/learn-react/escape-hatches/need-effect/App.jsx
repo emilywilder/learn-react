19ff091672f678ddc6c8d295444f9a2c0252e4d7
Emily Lockhart <validitymaiden@gmail.com>
2023-10-30 17:53:55 -0400

    fix bg settings for product image

--- pages/learn-react/escape-hatches/need-effect/App.jsx
+++ pages/learn-react/escape-hatches/need-effect/App.jsx
@@ -45,7 +45,7 @@ function Button({ onClick, children }) {
 function RenderProduct({ product, onByClick, onCheckoutClick }) {
     return (
         <div className="flex flex-col w-64">
-            <div className="h-full w-full bg-contain rounded bg-[url('/chester-alvarez-bphc6kyobMg-unsplash.jpg')]">
+            <div className="h-full w-full bg-cover rounded bg-[url('/chester-alvarez-bphc6kyobMg-unsplash.jpg')]">
                 <div className="h-full backdrop-brightness-50 rounded-lg">
                     <div className="text-white text-3xl p-2">
                         {product.name}
