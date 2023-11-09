026e28d0acad1352f2ee0c94a3e68d3a14c80138
Emily Lockhart <validitymaiden@gmail.com>
2023-11-09 11:53:07 -0500

    note the list of sublessons and unique names for each add Updating add list of sublessons to iterate with key, title, and component create a menu within the navbar showing each sublesson (currently just the two)

--- pages/learn-react/escape-hatches/need-effect/index.jsx
+++ pages/learn-react/escape-hatches/need-effect/index.jsx
@@ -2,7 +2,37 @@ import { StrictMode } from "react"
 
 import '../../../../app/globals.css'
 
+/* Pages and unique names
+    Updating: Updating state based on props or state
+    Caching: Caching expensive calculations
+    Resetting: Resetting all state when a prop changes
+    Adjusting: Adjusting some state when a prop changes
+    Sharing: Sharing logic between event handlers
+    Post: Sending a POST request
+    Chains: Chains of computations
+    Initializing: Initializing the application
+    Notifying: Notifying parent components about state changes
+    Passing: Passing data to the parent
+    Subscribing: Subscribing to an external store
+    Fetching: Fetching data
+*/
+
+import Updating from './Updating'
 import Sharing from './Sharing'
+
+const sublessons = [
+    {
+        id: 0,
+        title: "Updating state based on props or state",
+        component: Updating
+    },
+    {
+        id: 5,
+        title: "Sharing logic between event handlers",
+        component: Sharing
+
+    }
+]
 
 function Blank() {
     return (
@@ -11,20 +41,31 @@ function Blank() {
 }
 
 function NavBar() {
+    const listSublessons = (
+        sublessons.map((sl) => (
+            <li key={sl.id}><a>{sl.title}</a></li>
+        ))
+    )
+
     return (
-        <div className="navbar bg-base-100">
-            <div className="flex-none">
-                <button className="btn btn-square btn-ghost">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-                </button>
+        <>
+            <div className="navbar bg-base-100">
+                <div className="flex-none">
+                    <button className="btn btn-square btn-ghost">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                    </button>
+                </div>
+                <div className="flex-1 breadcrumbs ml-2">
+                    <ul>
+                        <li className="normal-case text-xl">You Might Not Need an Effect</li>
+                        <li>Sharing logic between event handlers</li>
+                    </ul>
+                </div>
             </div>
-            <div className="flex-1 breadcrumbs ml-2">
-                <ul>
-                    <li className="normal-case text-xl">You Might Not Need an Effect</li>
-                    <li>Sharing logic between event handlers</li>
-                </ul>
-            </div>
-        </div>
+            <ul className="menu bg-base-200 rounded-box">
+                {listSublessons}
+            </ul>
+        </>
     )
 }
 
