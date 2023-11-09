0db8869a4876d83d3f7bb4d8659b3b533098e240
Emily Lockhart <validitymaiden@gmail.com>
2023-11-09 11:13:20 -0500

    add navbar skeleton with current location

--- pages/learn-react/escape-hatches/need-effect/index.jsx
+++ pages/learn-react/escape-hatches/need-effect/index.jsx
@@ -1,9 +1,8 @@
 import { StrictMode } from "react"
 
 import '../../../../app/globals.css'
-// import "./styles.css"
 
-import App from './App'
+import Sharing from './Sharing'
 
 function Blank() {
     return (
@@ -11,10 +10,29 @@ function Blank() {
     )
 }
 
+function NavBar() {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="flex-none">
+                <button className="btn btn-square btn-ghost">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                </button>
+            </div>
+            <div className="flex-1 breadcrumbs ml-2">
+                <ul>
+                    <li className="normal-case text-xl">You Might Not Need an Effect</li>
+                    <li>Sharing logic between event handlers</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
 export default function Home() {
     return (
         <StrictMode>
-            <App />
+            <NavBar />
+            <Sharing />
         </StrictMode>
     )
 }
