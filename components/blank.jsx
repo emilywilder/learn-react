7e78c44983704c43f6a6c93b3192402e9ebda8b3
Emily Lockhart <validitymaiden@gmail.com>
2023-11-11 16:08:02 -0500

    add cat and butterfly image from unsplash dress up Blank component

--- components/blank.jsx
+++ components/blank.jsx
@@ -1,5 +1,15 @@
+import Image from 'next/image'
+
 export default function Blank() {
     return (
-        <p>This app is yet to be defined.</p>
+        <div className="pt-10 flex justify-center">
+            <div className="card shadow-xl overflow-hidden">
+                <div className="w-full h-80 bg-cover bg-center bg-[url('/karina-vorozheeva-rW-I87aPY5Y-unsplash.jpg')]" />
+                <div className="card-body">
+                    <h2 className="card-title justify-center">Oh hai ≽^•⩊•^≼</h2>
+                    <p>This app is yet to be defined.</p>
+                </div>
+            </div>
+        </div>
     )
 }
