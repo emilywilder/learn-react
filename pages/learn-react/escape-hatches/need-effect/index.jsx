277b15a445c093a24a92260ce5cad3d3e392c2b8
Emily Lockhart <validitymaiden@gmail.com>
2023-11-11 15:09:23 -0500

    fill out the rest of the sublessons and use a blank component for ones that have not been implemented yet

--- pages/learn-react/escape-hatches/need-effect/index.jsx
+++ pages/learn-react/escape-hatches/need-effect/index.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react"
 
 import '@/app/globals.css'
 import LessonNavbar from "@/components/lessonNavbar"
+import Blank from "@/components/blank"
 
 import Updating from './Updating'
 import Sharing from './Sharing'
@@ -14,21 +15,6 @@ export default function Home() {
     )
 }
 
-/* Pages and unique names
-    Updating: Updating state based on props or state
-    Caching: Caching expensive calculations
-    Resetting: Resetting all state when a prop changes
-    Adjusting: Adjusting some state when a prop changes
-    Sharing: Sharing logic between event handlers
-    Post: Sending a POST request
-    Chains: Chains of computations
-    Initializing: Initializing the application
-    Notifying: Notifying parent components about state changes
-    Passing: Passing data to the parent
-    Subscribing: Subscribing to an external store
-    Fetching: Fetching data
-*/
-
 const sublessons = [
     {
         id: 0,
@@ -37,9 +23,69 @@ const sublessons = [
         usesTailwind: false
     },
     {
-        id: 5,
+        id: 1,
+        title: "Caching expensive calculations",
+        component: Blank, // Caching
+        usesTailwind: false
+    },
+    {
+        id: 2,
+        title: "Resetting all state when a prop changes",
+        component: Blank, // Resetting
+        usesTailwind: false
+    },
+    {
+        id: 3,
+        title: "Adjusting some state when a prop changes",
+        component: Blank, // Adjusting
+        usesTailwind: false
+    },
+    {
+        id: 4,
         title: "Sharing logic between event handlers",
         component: Sharing,
         usesTailwind: true
-    }
+    },
+    {
+        id: 5,
+        title: "Sending a POST request",
+        component: Blank, // Post
+        usesTailwind: true
+    },
+    {
+        id: 6,
+        title: "Chains of computations",
+        component: Blank, // Chains
+        usesTailwind: true
+    },
+    {
+        id: 7,
+        title: "Initializing the application",
+        component: Blank, // Initializing
+        usesTailwind: true
+    }, 
+    {
+        id: 8,
+        title: "Notifying parent components about state changes",
+        component: Blank, // Notifying
+        usesTailwind: true
+    },
+    {
+        id: 9,
+        title: "Passing data to the parent",
+        component: Blank, // Passing
+        usesTailwind: true
+    },
+    {
+        id: 10,
+        title: "Subscribing to an external store",
+        component: Blank, // Subscribing
+        usesTailwind: true
+    }, 
+    {
+        id: 11,
+        title: "Fetching data",
+        component: Blank, // Fetching
+        usesTailwind: true
+    },
 ]
