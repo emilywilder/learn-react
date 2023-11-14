e469d3632478cd965a5c8244a2a05d28cdf3a902
Emily Lockhart <validitymaiden@gmail.com>
2023-11-13 19:40:34 -0500

    move registry list into its own component title as event rather than the registry use a grid rather than a flex box for relating the registry list and registration form change form button to say register rather than submit, so as to make explicity the purpose of the form

--- pages/learn-react/escape-hatches/need-effect/post/App.jsx
+++ pages/learn-react/escape-hatches/need-effect/post/App.jsx
@@ -42,7 +42,7 @@ function Form() {
                 />
                 <div className="form-control mt-6">
                     <button className="btn btn-primary" onClick={handleSubmit}>
-                        Submit
+                        Register
                     </button>
                 </div>
             </form>
@@ -98,50 +98,60 @@ function Registry() {
     }
 
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex lg:flex-row space-x-10">
-                <div className="text-center">
-                    <h1 className="text-5xl font-bold">Registry</h1>
-                    <div className="py-6 overflow-x-hidden">
-                        {registryList.length ? (
-                            <table className="table bg-base-100">
-                                <thead>
-                                    <tr>
-                                        <th></th>
-                                        <th>Name</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {registryList.map(n => (
-                                        <tr key={n.id}>
-                                            <th>{n.id + 1}</th>
-                                            <td>{n.firstName} {n.lastName}</td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        ) : (
-                            <p>There are currently no names registered.</p>
-                        )}
+        <div className="min-h-screen bg-base-200">
+            <div className="hero-content flex-col">
+                <div className="text-5xl font-bold p-10">Amazing Event</div>
+                <div className="grid grid-cols-2 space-x-4">
+                    <div className="self-start">
+                        <RegistryList registryList={registryList}/>
+                    </div>
+                    <div className="space-y-5 self-end">
+                        <formContext.Provider value={post}>
+                            <Form />
+                        </formContext.Provider>
+                        <div className="flex justify-end">
+                            <label className="label space-x-4">
+                                <span className="label-text font-thin">Analytics sent</span> 
+                                <input
+                                    className="checkbox checkbox-xs checkbox-secondary"
+                                    type="checkbox" 
+                                    checked={analyticsSent}
+                                    readOnly
+                                />
+                            </label>
+                        </div>
                     </div>
                 </div>
-                
-                <div className="flex flex-col space-y-5">
-                    <formContext.Provider value={post}>
-                        <Form />
-                    </formContext.Provider>
-                    <div className="flex justify-end">
-                        <label className="label space-x-4">
-                            <span className="label-text font-thin">Analytics sent</span> 
-                            <input
-                                className="checkbox checkbox-xs checkbox-secondary"
-                                type="checkbox" 
-                                checked={analyticsSent}
-                                readOnly
-                            />
-                        </label>
-                    </div>
-                </div>
+            </div>
+        </div>
+    )
+}
+
+function RegistryList({registryList}) {
+    return (
+        <div className="flex flex-col self-start">
+            <h1 className="font-bold">Registry list</h1>
+            <div className="py-6 overflow-x-hidden">
+                {registryList.length ? (
+                    <table className="table bg-base-100">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {registryList.map(n => (
+                                <tr key={n.id}>
+                                    <th>{n.id + 1}</th>
+                                    <td>{n.firstName} {n.lastName}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                ) : (
+                    <p>There are currently no names registered.</p>
+                )}
             </div>
         </div>
     )
