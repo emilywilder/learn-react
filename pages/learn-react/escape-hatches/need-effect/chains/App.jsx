89fee960da582dd483cc6ebef9836c2e641ad3bc
Emily Lockhart <validitymaiden@gmail.com>
2023-12-05 14:41:13 -0500

    add cards and show a row of 3 blank spaces and a row of cards to choose from

--- pages/learn-react/escape-hatches/need-effect/chains/App.jsx
+++ pages/learn-react/escape-hatches/need-effect/chains/App.jsx
@@ -1,4 +1,26 @@
+import Image from "next/image";
 import { useEffect, useState } from "react";
+
+const cards = [
+    {
+        id: 0,
+        name: 'chariot',
+        location: '/images/chariot-6016921_1280.jpg',
+        gold: false,
+    },
+    {
+        id: 1,
+        name: 'empress',
+        location: '/images/empress-6016923_1280.jpg',
+        gold: false,
+    },
+    {
+        id: 2,
+        name: 'the hanged man',
+        location: '/images/hanged-man-6016939_1280.jpg',
+        gold: false,
+    },
+]
 
 function Game() {
     const [card, setCard] = useState(null)
@@ -39,8 +61,52 @@ function Game() {
     }
 
     // ...
+    return (
+        <div className="flex justify-center">
+            <div className="grid grid-cols-3 grid-rows-2 my-10">
+                <Blank /><Blank /><Blank />
+                <Card id={0} />
+                <Card id={1} />
+                <Card id={2} />
+            </div>
+        </div>
+    )
+}
+
+function Blank() {
+    return (
+        <CardSpace>
+            <div className="h-full border-4 rounded-xl border-black flex items-center justify-center bg-base-100">
+                Place a card here.
+            </div>
+        </CardSpace>
+    )
+}
+
+function CardSpace({ children }) {
+    return (
+        <div className="w-48 h-128 m-4">
+            {children}
+        </div>
+    )
+}
+
+function Card({ id }) {
+    const card = cards.find(c => c.id === id)
+    return (
+        <CardSpace>
+            <button className="card shadow-xl place-items-center">
+                <Image width={720} height={1280} src={card.location} alt={card.name} />
+            </button>
+        </CardSpace>
+    )
 }
 
 export default function App() {
-
+    return (
+        <div className="bg-base-200 h-screen">
+            <Game />
+        </div>
+    )
+    
 }
