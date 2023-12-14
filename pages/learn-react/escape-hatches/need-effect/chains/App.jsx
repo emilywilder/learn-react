4e52ba110c0f113e586174f05790fa5e76b054fb
Emily Lockhart <validitymaiden@gmail.com>
2023-12-14 18:52:07 -0500

    Add 'good' implementation from example

--- pages/learn-react/escape-hatches/need-effect/chains/App.jsx
+++ pages/learn-react/escape-hatches/need-effect/chains/App.jsx
@@ -100,6 +100,37 @@ function BadGame() {
     )
 }
 
+function GoodGame() {
+    const [card, setCard] = useState(null)
+    const [goldCardCount, setGoldCardCount] = useState(0)
+    const [round, setRound] = useState(1)
+  
+    // ✅ Calculate what you can during rendering
+    const isGameOver = round > 5
+  
+    function handlePlaceCard(nextCard) {
+        if (isGameOver) {
+            throw Error('Game already ended.')
+        }
+  
+        // ✅ Calculate all the next state in the event handler
+        setCard(nextCard)
+        if (nextCard.gold) {
+            if (goldCardCount <= 3) {
+                setGoldCardCount(goldCardCount + 1)
+            } else {
+                setGoldCardCount(0)
+                setRound(round + 1)
+                if (round === 5) {
+                    alert('Good game!')
+                }
+            }
+        }
+    }
+
+    // ...
+}
+
 function RenderGame({round, goldCardCount, handlePlaceCard, isGameOver, alertMsgs }) {
     const [cards, setCards] = useState(newCards())
     const [showRound, setShowRound] = useState(true)
