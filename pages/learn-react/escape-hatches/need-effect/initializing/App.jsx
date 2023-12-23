eeb18beaffa69d589b8a6d589f5d4e79c31aef0e
Emily Lockhart <validitymaiden@gmail.com>
2023-12-23 15:46:55 -0500

    switch to use react-toastify since toastify-js wasn't handling the inital toasts as well. ensure the text content doesn't overlap the toasts

--- pages/learn-react/escape-hatches/need-effect/initializing/App.jsx
+++ pages/learn-react/escape-hatches/need-effect/initializing/App.jsx
@@ -1,27 +1,15 @@
 import { useEffect, useState } from "react";
-import Toastify from 'toastify-js'
-import 'toastify-js/src/toastify.css'
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 let didInit = false
 
-function toast(message) {
-    Toastify({
-        text: message,
-        duration: 2000,
-        gravity: 'top',
-        position: 'right',
-        offset: {
-            y: 90
-        }
-    }).showToast()
-}
-
 function loadDataFromLocalStorage() {
-    toast('Loading data from storage')
-}
+    toast('Loading data from storage...')
+}   
 
 function checkAuthToken() {
-    toast('Checking auth token')
+    toast('Checking auth token...')
 }
 
 function UseEffectApp() {
@@ -65,28 +53,39 @@ function LoadUnloadApp({ stateVar, setStateVar, App, label="" }) {
 export default function App() {
     const [loadUseEffectApp, setLoadUseEffectApp] = useState(false)
     const [loadGobalVarApp, setLoadGlobalVarApp] = useState(false)
+    
+    const toastWidth = '260px'
 
     return (
-        <div className="m-10 w-1/2 space-y-6">
-            <div className="text-lg font-bold text-blue-500">Demonstrate implementation differences in running initialization functions.</div>
-            <p>
-                On page load the initialization functions run one time, prior to react component mounting.
-            </p>
-            <p>
-                This component runs the functions in a useEffect() call. Since useEffect() is run
-                on mount, the functions inside are called every time the component is loaded.
-            </p>
-            <LoadUnloadApp stateVar={loadUseEffectApp} setStateVar={setLoadUseEffectApp}
-                App={UseEffectApp} label="useEffect in component"
-            />
-            <div>
-                This component also runs the functions in a useEffect() call, but utilizes a global variable
-                to keep track of whether the function has already been run. This results in the functions
-                being run only once.
+        <div className="flex flex-row-reverse ">
+            {/* space for toasts to be visible */}
+            <div className={`min-w-[${toastWidth}]`}>
+                <ToastContainer style={{
+                    '--toastify-toast-width': toastWidth,
+                    'top': '100px',
+                }}/>
             </div>
-            <LoadUnloadApp stateVar={loadGobalVarApp} setStateVar={setLoadGlobalVarApp}
-                App={GlobalVarApp} label="useEffect with global variable"
-            />
+            <div className="m-10 space-y-6">
+                <div className="text-lg font-bold text-blue-500">Demonstrate implementation differences in running initialization functions.</div>
+                <p>
+                    On page load the initialization functions run one time, prior to react component mounting.
+                </p>
+                <p>
+                    This component runs the functions in a useEffect() call. Since useEffect() is run
+                    on mount, the functions inside are called every time the component is loaded.
+                </p>
+                <LoadUnloadApp stateVar={loadUseEffectApp} setStateVar={setLoadUseEffectApp}
+                    App={UseEffectApp} label="useEffect in component"
+                />
+                <div>
+                    This component also runs the functions in a useEffect() call, but utilizes a global variable
+                    to keep track of whether the function has already been run. This results in the functions
+                    being run only once.
+                </div>
+                <LoadUnloadApp stateVar={loadGobalVarApp} setStateVar={setLoadGlobalVarApp}
+                    App={GlobalVarApp} label="useEffect with global variable"
+                />
+            </div>
         </div>
     )
 }
