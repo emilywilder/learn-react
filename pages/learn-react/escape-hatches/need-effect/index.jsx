51e836c8f3adda2e8dec9bba2922595d6aba6075
Emily Lockhart <validitymaiden@gmail.com>
2024-01-16 15:28:23 -0500

    save prettier changes

--- pages/learn-react/escape-hatches/need-effect/index.jsx
+++ pages/learn-react/escape-hatches/need-effect/index.jsx
@@ -1,17 +1,17 @@
 import { StrictMode } from "react"
 
-import '@/app/globals.css'
+import "@/app/globals.css"
 import LessonNavbar from "@/components/lessonNavbar"
 
-import Updating from './updating/App'
-import Caching from './caching/App'
-import Resetting from './resetting/App'
-import Adjusting from './adjusting/App'
-import Sharing from './sharing/App'
-import Post from './post/App'
-import Chains from './chains/App'
-import Initializing from './initializing/App'
-import Notifying from './notifying/App'
+import Updating from "./updating/App"
+import Caching from "./caching/App"
+import Resetting from "./resetting/App"
+import Adjusting from "./adjusting/App"
+import Sharing from "./sharing/App"
+import Post from "./post/App"
+import Chains from "./chains/App"
+import Initializing from "./initializing/App"
+import Notifying from "./notifying/App"
 
 export default function Home() {
     return (
@@ -26,72 +26,72 @@ const sublessons = [
         id: 0,
         title: "Updating state based on props or state",
         component: Updating,
-        usesTailwind: false
+        usesTailwind: false,
     },
     {
         id: 1,
         title: "Caching expensive calculations",
         component: Caching,
-        usesTailwind: false
+        usesTailwind: false,
     },
     {
         id: 2,
         title: "Resetting all state when a prop changes",
         component: Resetting,
-        usesTailwind: true
+        usesTailwind: true,
     },
     {
         id: 3,
         title: "Adjusting some state when a prop changes",
         component: Adjusting,
-        usesTailwind: true
+        usesTailwind: true,
     },
     {
         id: 4,
         title: "Sharing logic between event handlers",
         component: Sharing,
-        usesTailwind: true
+        usesTailwind: true,
     },
     {
         id: 5,
         title: "Sending a POST request",
         component: Post,
-        usesTailwind: true
+        usesTailwind: true,
     },
     {
         id: 6,
         title: "Chains of computations",
         component: Chains,
-        usesTailwind: true
+        usesTailwind: true,
     },
     {
         id: 7,
         title: "Initializing the application",
         component: Initializing,
-        usesTailwind: true
-    }, 
+        usesTailwind: true,
+    },
     {
         id: 8,
         title: "Notifying parent components about state changes",
         component: Notifying,
-        usesTailwind: true
+        usesTailwind: true,
     },
     {
         id: 9,
         title: "Passing data to the parent",
         component: null, // Passing
-        usesTailwind: true
+        usesTailwind: true,
     },
     {
         id: 10,
         title: "Subscribing to an external store",
         component: null, // Subscribing
-        usesTailwind: true
-    }, 
+        usesTailwind: true,
+    },
     {
         id: 11,
         title: "Fetching data",
         component: null, // Fetching
-        usesTailwind: true
+        usesTailwind: true,
     },
 ]
