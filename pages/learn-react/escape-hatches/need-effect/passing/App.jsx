0ae1d96d77cadc00d2f7c316b67e6fcff437adf3
Emily Lockhart <validitymaiden@gmail.com>
2024-01-16 15:43:05 -0500

    add examples from tutorial

--- pages/learn-react/escape-hatches/need-effect/passing/App.jsx
+++ pages/learn-react/escape-hatches/need-effect/passing/App.jsx
@@ -1,1 +1,29 @@
+function UpstreamParent() {
+    const [data, setData] = useState(null)
+    // ...
+    return <UpstreamChild onFetched={setData} />
+}
+
+function UpstreamChild({ onFetched }) {
+    const data = useSomeAPI()
+    // 🔴 Avoid: Passing data to the parent in an Effect
+    useEffect(() => {
+        if (data) {
+            onFetched(data)
+        }
+    }, [onFetched, data])
+    // ...
+}
+
+function DownstreamParent() {
+    const data = useSomeAPI()
+    // ...
+    // ✅ Good: Passing data down to the child
+    return <DownstreamChild data={data} />
+}
+
+function DownstreamChild({ data }) {
+    // ...
+}
+
 export default function App() {}
