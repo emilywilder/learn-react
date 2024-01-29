19b07177cc04b03893286ee9b327cbd2dc4cd229
Emily Lockhart <validitymaiden@gmail.com>
2024-01-29 18:45:27 -0500

    move miragejs server to utilities

--- pages/learn-react/escape-hatches/need-effect/fetching/App.jsx
+++ pages/learn-react/escape-hatches/need-effect/fetching/App.jsx
@@ -5,24 +5,6 @@ import HookFetch from "./HookFetch"
 const paramsString =
     "sublesson=fetching&lesson=need-effect&topic=escape-hatches"
 
-import { createServer } from "miragejs"
-
-createServer({
-    routes() {
-        // namespace issue resolution taken from
-        // https://github.com/miragejs/miragejs/issues/651#issuecomment-713283963
-        this.namespace = "api"
-        this.get("/search", (schema, request) => {
-            const page = request.queryParams.page
-            const query = request.queryParams.query
-            const queryObj = Object.fromEntries(new URLSearchParams(query))
-            return queryObj
-        })
-        this.namespace = ""
-        this.passthrough()
-    },
-})
-
 export default function Fetching() {
     return (
         <>
--- pages/learn-react/escape-hatches/need-effect/fetching/utilities.js
+++ pages/learn-react/escape-hatches/need-effect/fetching/utilities.js
@@ -1,3 +1,21 @@
+import { createServer } from "miragejs"
+
+createServer({
+    routes() {
+        // namespace issue resolution taken from
+        // https://github.com/miragejs/miragejs/issues/651#issuecomment-713283963
+        this.namespace = "api"
+        this.get("/search", (schema, request) => {
+            const page = request.queryParams.page
+            const query = request.queryParams.query
+            const queryObj = Object.fromEntries(new URLSearchParams(query))
+            return queryObj
+        })
+        this.namespace = ""
+        this.passthrough()
+    },
+})
+
 export async function fetchResults(query, page) {
     const params = new URLSearchParams({ query, page })
     console.log(params)
