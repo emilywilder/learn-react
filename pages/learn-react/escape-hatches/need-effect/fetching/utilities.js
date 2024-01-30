e11d772f4fc0cec7bbd2acd9b00fde428e20f869
Emily Lockhart <validitymaiden@gmail.com>
2024-01-29 20:10:58 -0500

    have fetchResults return a reconrd from a book catalogue based on the page requested

--- pages/learn-react/escape-hatches/need-effect/fetching/utilities.js
+++ pages/learn-react/escape-hatches/need-effect/fetching/utilities.js
@@ -3,6 +3,14 @@ import { createServer } from "miragejs"
 export const PARAMS_STRING =
     "sublesson=fetching&lesson=need-effect&topic=escape-hatches"
 
+const BOOK_CATALOGUE = [
+    { id: 1, title: "First Book", author: "One Oneington" },
+    { id: 2, title: "Second Book", author: "Two Twostone" },
+    { id: 3, title: "Third Book", author: "Three Threeple" },
+    { id: 4, title: "Fourth Book", author: "Four Fourier" },
+    { id: 5, title: "Fifth Book", author: "Five Fivety" },
+]
+
 createServer({
     routes() {
         // namespace issue resolution taken from
@@ -24,7 +32,7 @@ createServer({
 export async function fetchResults(query, page) {
     console.debug(`fetchResults(): ${query}`)
     if (query === PARAMS_STRING) {
-        return page
+        return BOOK_CATALOGUE.find((x) => x.id === Number(page))
     } else {
         throw Error(`Unknown query: ${query}`)
     }
