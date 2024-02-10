81c97789b6e7387ce10a5d9304a8c1f2cc6a3bfe
Emily Lockhart <validitymaiden@gmail.com>
2024-02-10 17:13:05 -0500

    use <Lesson> for escape-hatches/lifecycle index

--- pages/learn-react/escape-hatches/lifecycle/index.jsx
+++ pages/learn-react/escape-hatches/lifecycle/index.jsx
@@ -1,25 +1,8 @@
-import { StrictMode } from "react"
-
-import "@/app/globals.css"
-import LessonNavbar from "@/components/lessonNavbar"
+import Lesson from "@/components/Lesson"
 
 export default function Home() {
-    const mostRecentSublesson = sublessons
-        .filter((x) => x.component)
-        .reduce(
-            (previous, current) =>
-                current.id > previous.id ? current : previous,
-            []
-        )
-
     return (
-        <StrictMode>
-            <LessonNavbar
-                lessonName="Lifecycle of Reactive Effects"
-                sublessons={sublessons}
-                defaultSelectedId={mostRecentSublesson.id}
-            />
-        </StrictMode>
+        <Lesson name="Lifecycle of Reactive Effects" sublessons={sublessons} />
     )
 }
 
