89e859ed02fc5b9b9f0f19dd33a050d9ed0e1287
Emily Lockhart <validitymaiden@gmail.com>
2024-02-10 15:46:20 -0500

    formatting changes to <Blank>

--- components/blank.jsx
+++ components/blank.jsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from "next/image"
 
 export default function Blank() {
     return (
@@ -6,7 +6,9 @@ export default function Blank() {
             <div className="card shadow-xl overflow-hidden">
                 <div className="w-full h-80 bg-cover bg-center bg-[url('/images/karina-vorozheeva-rW-I87aPY5Y-unsplash.jpg')]" />
                 <div className="card-body">
-                    <h2 className="card-title justify-center">Oh hai ≽^•⩊•^≼</h2>
+                    <h2 className="card-title justify-center">
+                        Oh hai ≽^•⩊•^≼
+                    </h2>
                     <p>This app is yet to be defined.</p>
                 </div>
             </div>
