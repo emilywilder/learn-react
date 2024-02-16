9bef083f575bd5add6ad8d54606cd134a42b9cb6
Emily Lockhart <validitymaiden@gmail.com>
2024-02-16 12:35:09 -0500

    remove unused imports

--- pages/learn-react/escape-hatches/lifecycle/effectLifecycle/App.jsx
+++ pages/learn-react/escape-hatches/lifecycle/effectLifecycle/App.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 
 function createConnection(serverUrl, roomId) {
     const identifier = `${serverUrl}:${roomId}`
