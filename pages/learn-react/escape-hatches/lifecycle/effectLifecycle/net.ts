accf161f825862ffcf0b45e4a89b692c6b17f54f
Emily Lockhart <validitymaiden@gmail.com>
2024-02-16 16:28:56 -0500

    convert net.js to typescript in net.ts

---
+++ pages/learn-react/escape-hatches/lifecycle/effectLifecycle/net.ts
@@ -0,0 +1,71 @@
+import { useSyncExternalStore } from "react"
+
+export const serverUrl: string = "https://localhost:1234"
+
+// Method for using useSyncExternalStore with globals from
+// https://javascript.plainenglish.io/usesyncexternalstore-to-manage-global-state-in-react-ac31c7191376
+
+let CHAT_CONNECTIONS: Array<number> = []
+const CHAT_CALLBACKS: Array<Function> = []
+
+function addConnection(roomId: number): void {
+    CHAT_CONNECTIONS = [...CHAT_CONNECTIONS, roomId]
+    notify()
+}
+
+function removeConnection(roomId: number): void {
+    CHAT_CONNECTIONS = CHAT_CONNECTIONS.filter((id) => id !== roomId)
+    notify()
+}
+
+function notify(): void {
+    CHAT_CALLBACKS.forEach((cb) => cb())
+}
+
+export function subscribe(cb: () => void): () => void {
+    CHAT_CALLBACKS.push(cb)
+    return () => {
+        const index: number = CHAT_CALLBACKS.indexOf(cb)
+        if (index > -1) {
+            CHAT_CALLBACKS.splice(index, 1)
+        }
+    }
+}
+
+export function useOnlineStatus(roomId: number): boolean {
+    function getSnapshot(): boolean {
+        return CHAT_CONNECTIONS.find((x) => x === roomId) ? true : false
+    }
+
+    const store = useSyncExternalStore(
+        subscribe,
+        getSnapshot, // client
+        () => true // server
+    )
+
+    return store
+}
+
+interface ConnectionType {
+    connect: Function
+    disconnect: Function
+}
+
+export function createConnection(
+    serverUrl: string,
+    roomId: number
+): ConnectionType {
+    const identifier = `${serverUrl}:${roomId}`
+
+    const connect = () => {
+        addConnection(roomId)
+        console.log(`connected to ${identifier}`)
+    }
+
+    const disconnect = () => {
+        removeConnection(roomId)
+        console.log(`disconnected from ${identifier}`)
+    }
+
+    return { connect: connect, disconnect: disconnect }
+}
