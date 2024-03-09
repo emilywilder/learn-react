024f0ca7d898fb4bf99af94ef343753d89964a83
Emily Lockhart <validitymaiden@gmail.com>
2024-03-09 14:35:47 -0500

    copy app from HowResync

--- modules/learn-react/escape-hatches/lifecycle/syncProcess/App.jsx
+++ modules/learn-react/escape-hatches/lifecycle/syncProcess/App.jsx
@@ -1,1 +1,30 @@
-export default function App() {}
+import { useState } from "react"
+import { RoomMenu, ChatRoom } from "./ChatRoom"
+
+export default function HowResync() {
+    const [selected, setSelected] = useState(0)
+    const [show, setShow] = useState(true)
+
+    return (
+        <>
+            <div className="fixed top-24 right-14">
+                <div className="flex space-x-2">
+                    <RoomMenu setSelected={setSelected} />
+                    <button
+                        className={`btn ${
+                            show ? "btn-error" : "btn-info"
+                        } text-white`}
+                        onClick={() => setShow(!show)}
+                    >
+                        {show ? "Close" : "Open"} Chatroom
+                    </button>
+                </div>
+            </div>
+            {show && (
+                <div className="flex m-10">
+                    <ChatRoom roomId={selected} />
+                </div>
+            )}
+        </>
+    )
+}
