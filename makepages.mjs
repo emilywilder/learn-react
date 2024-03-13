104e258b0c17d0099b4738390c2e4825d7b3bf3e
Emily Lockhart <validitymaiden@gmail.com>
2024-03-13 14:38:48 -0400

    convert makepages to an esm file

--- makepages.mjs
+++ makepages.mjs
@@ -1,21 +1,21 @@
-const fs = require("fs/promises")
-const path = require("path")
+import { readFile, mkdir, writeFile } from "fs/promises"
+import { join } from "path"
 
 const apps_file = "app/apps.json"
 
 async function makePages() {
-    const apps_data_raw = await fs.readFile(apps_file, "utf8")
+    const apps_data_raw = await readFile(apps_file, "utf8")
     const apps_data = JSON.parse(apps_data_raw)
     for (const part of apps_data.parts) {
         for (const section of part.sections) {
             for (const app of section.apps) {
-                const page_dir = path.join("pages", app.location)
+                const page_dir = join("pages", app.location)
                 console.log(`Creating ${page_dir}...`)
-                await fs.mkdir(page_dir, { recursive: true })
-                const index_dir = path.join("@", "modules", app.location)
+                await mkdir(page_dir, { recursive: true })
+                const index_dir = join("@", "modules", app.location)
                 const content = `import App from "${index_dir}"\nexport default App\n`
                 console.log(`Writing index for ${page_dir}...`)
-                await fs.writeFile(path.join(page_dir, "index.jsx"), content)
+                await writeFile(join(page_dir, "index.jsx"), content)
             }
         }
     }
