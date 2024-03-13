e65616d8245ea72d2b11c5297c92686eae2a3417
Emily Lockhart <validitymaiden@gmail.com>
2024-03-12 22:15:26 -0400

    add lifecycle/challenge4 alternate solution

--- modules/learn-react/escape-hatches/lifecycle/challenge4/ChatRoom.js
+++ modules/learn-react/escape-hatches/lifecycle/challenge4/ChatRoom.js
@@ -1,11 +1,19 @@
 import { useState, useEffect } from "react"
 
-export default function ChatRoom({ roomId, createConnection }) {
+import {
+    createEncryptedConnection,
+    createUnencryptedConnection,
+} from "./chat.js"
+
+export default function ChatRoom({ roomId, isEncrypted }) {
     useEffect(() => {
+        const createConnection = isEncrypted
+            ? createEncryptedConnection
+            : createUnencryptedConnection
         const connection = createConnection(roomId)
         connection.connect()
         return () => connection.disconnect()
-    }, [roomId, createConnection])
+    }, [roomId, isEncrypted])
 
     return <h1>Welcome to the {roomId} room!</h1>
 }
