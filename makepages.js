ca0fad50f6ebf707e8c3c0fad74e0d268bf5d931
Emily Lockhart <validitymaiden@gmail.com>
2024-03-13 14:32:39 -0400

    use async/await and for/of

--- makepages.js
+++ makepages.js
@@ -3,24 +3,26 @@ const path = require("path")
 
 const apps_file = "app/apps.json"
 
-fs.readFile(apps_file, "utf8").then((_data) => {
-    const data = JSON.parse(_data)
-    data.parts.forEach((part) => {
-        part.sections.forEach((section) => {
-            section.apps.forEach((app) => {
+async function makePages() {
+    const apps_data_raw = await fs.readFile(apps_file, "utf8")
+    const apps_data = JSON.parse(apps_data_raw)
+    for (const part of apps_data.parts) {
+        for (const section of part.sections) {
+            for (const app of section.apps) {
                 const page_dir = path.join("pages", app.location)
+                console.log(`Creating ${page_dir}...`)
+                await fs.mkdir(page_dir, { recursive: true })
                 const index_dir = path.join("@", "modules", app.location)
-                try {
-                    console.log(`Creating ${page_dir}...`)
-                    fs.mkdir(page_dir, { recursive: true }).then(() => {
-                        const content = `import App from "${index_dir}"\nexport default App\n`
-                        console.log(`Writing index for ${page_dir}...`)
-                        fs.writeFile(path.join(page_dir, "index.jsx"), content)
-                    })
-                } catch (err) {
-                    console.error(err)
-                }
-            })
-        })
-    })
-})
+                const content = `import App from "${index_dir}"\nexport default App\n`
+                console.log(`Writing index for ${page_dir}...`)
+                await fs.writeFile(path.join(page_dir, "index.jsx"), content)
+            }
+        }
+    }
+}
+
+try {
+    makePages()
+} catch (err) {
+    console.error(err)
+}
