cb3bcc0672f0b1a324b9aac5c7d3033fd0a64528
Emily Lockhart <validitymaiden@gmail.com>
2024-03-17 16:29:13 -0400

    have <ReadingLesson> utilize the new <HeroPage>

--- components/ReadingLesson.jsx
+++ components/ReadingLesson.jsx
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router"
 
+import HeroPage from "@/components/HeroPage"
+
 export default function ReadingLesson({ url }) {
     const router = useRouter()
 
@@ -7,37 +9,29 @@ export default function ReadingLesson({ url }) {
         router.push(url)
     }
     return (
-        <div
-            className="hero h-[calc(100vh-5em)]"
-            style={{
-                backgroundImage:
-                    "url(/images/jess-bailey-X5gDoysLbBc-unsplash.jpg)",
-            }}
+        <HeroPage
+            backgroundImage={"/images/jess-bailey-X5gDoysLbBc-unsplash.jpg"}
         >
-            <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello!</h1>
-                    <p className="mb-5">
-                        This is a reading lesson.
-                        <br />
-                        Please visit react.dev for this lesson&apos;s contents.
-                    </p>
-
-                    {url ? (
-                        <button
-                            className="btn btn-primary"
-                            onClick={handleHomeClick}
-                        >
-                            Take me there
-                        </button>
-                    ) : (
-                        <button className="btn" disabled>
-                            URL not specified
-                        </button>
-                    )}
-                </div>
-            </div>
-        </div>
+            <HeroPage.Title>Hello!</HeroPage.Title>
+            <HeroPage.Content>
+                This is a reading lesson.
+                <br />
+                Please visit react.dev for this lesson&apos;s contents.
+            </HeroPage.Content>
+            <HeroPage.Actions>
+                {url ? (
+                    <button
+                        className="btn btn-primary"
+                        onClick={handleHomeClick}
+                    >
+                        Take me there
+                    </button>
+                ) : (
+                    <button className="btn" disabled>
+                        URL not specified
+                    </button>
+                )}
+            </HeroPage.Actions>
+        </HeroPage>
     )
 }
