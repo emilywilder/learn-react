bab04d2ca5e8ce31384e5302622a0e15acc45e46
Emily Lockhart <validitymaiden@gmail.com>
2024-03-18 13:29:46 -0400

    add reading lessons for "Reading latest props and state with Effect Events" and "Limitations of Effect Events"

--- modules/learn-react/escape-hatches/separating/index.jsx
+++ modules/learn-react/escape-hatches/separating/index.jsx
@@ -2,6 +2,7 @@ import Lesson from "@/components/Lesson"
 import ReadingLesson from "@/components/ReadingLesson"
 
 import ExtractingNonReactive from "./extractingNonReactive/App"
+import { title } from "process"
 
 export default function Home() {
     return (
@@ -27,5 +28,17 @@ const sublessons = [
         title: "Extracting non-reactive logic out of Effects",
         component: ExtractingNonReactive,
         experimental: true,
+    },
+    {
+        id: 3,
+        title: "Reading latest props and state with Effect Events",
+        component: ReadingLesson,
+        url: "https://react.dev/learn/separating-events-from-effects#reading-latest-props-and-state-with-effect-events",
+    },
+    {
+        id: 4,
+        title: "Limitations of Effect Events",
+        component: ReadingLesson,
+        url: "https://react.dev/learn/separating-events-from-effects#limitations-of-effect-events",
     },
 ]
