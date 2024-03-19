9b319064f061d42b6aa8df0b4303b54a71a9d900
Emily Lockhart <validitymaiden@gmail.com>
2024-03-19 16:37:25 -0400

    create sublesson MoveToEvent

--- modules/learn-react/escape-hatches/removing/index.jsx
+++ modules/learn-react/escape-hatches/removing/index.jsx
@@ -1,6 +1,8 @@
 import Lesson from "@/components/Lesson"
 import ReadingLesson from "@/components/ReadingLesson"
 
+import MoveToEvent from "./moveToEvent/App"
+
 export default function Home() {
     return (
         <Lesson name="Removing Effect Dependencies" sublessons={sublessons} />
@@ -14,9 +16,13 @@ const sublessons = [
         component: ReadingLesson,
         url: "https://react.dev/learn/removing-effect-dependencies#dependencies-should-match-the-code",
     },
+    {
+        id: 1,
+        title: "Should this code move to an event handler?",
+        component: MoveToEvent,
+    },
 ]
 
-// MoveToEvent: Should this code move to an event handler?
 // Unrelated: Is your Effect doing several unrelated things?
 // Calculate: Are you reading some state to calculate the next state?
 // NotReacting: Do you want to read a value without “reacting” to its changes?
