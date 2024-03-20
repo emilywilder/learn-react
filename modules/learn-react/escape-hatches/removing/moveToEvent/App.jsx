b86eff2421ef745ae6eb90229bfb03ca6f0b9a76
Emily Lockhart <validitymaiden@gmail.com>
2024-03-20 11:35:21 -0400

    add menu button for theme selection

--- modules/learn-react/escape-hatches/removing/moveToEvent/App.jsx
+++ modules/learn-react/escape-hatches/removing/moveToEvent/App.jsx
@@ -4,8 +4,29 @@ import InEffectForm from "./InEffectForm"
 import InEventForm from "./InEventForm"
 import { useState } from "react"
 
+export function ThemeMenu({ options, setSelected }) {
+    return (
+        <details className="dropdown dropdown-bottom">
+            <summary className="btn btn-primary w-24">Themes</summary>
+            <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box">
+                {options.map((option) => (
+                    <li key={option.id}>
+                        <a onClick={() => setSelected(option.name)}>
+                            <span>{option.name}</span>
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </details>
+    )
+}
+
 function Card({ title, description, children }) {
-    const [theme, setTheme] = useState("light")
+    const themes = [
+        { id: 0, name: "light" },
+        { id: 1, name: "dark" },
+    ]
+    const [theme, setTheme] = useState(themes.find((t) => t.name === "light"))
     return (
         <ThemeContext.Provider value={theme}>
             <div className="card shadow-xl bg-base-100 w-72" data-theme={theme}>
@@ -13,10 +34,7 @@ function Card({ title, description, children }) {
                     <h1 className="card-title">{title}</h1>
                     <p>{description}</p>
                     <div className="card-actions justify-end">
-                        <select onChange={(e) => setTheme(e.target.value)}>
-                            <option>light</option>
-                            <option>dark</option>
-                        </select>
+                        <ThemeMenu options={themes} setSelected={setTheme} />
                         {children}
                     </div>
                 </div>
