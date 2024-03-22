90dae45f9ec990f308f8334202a7125bea178b83
Emily Lockhart <validitymaiden@gmail.com>
2024-03-22 12:49:26 -0400

    add Unrelated sublesson

--- modules/learn-react/escape-hatches/removing/index.jsx
+++ modules/learn-react/escape-hatches/removing/index.jsx
@@ -2,6 +2,7 @@ import Lesson from "@/components/Lesson"
 import ReadingLesson from "@/components/ReadingLesson"
 
 import MoveToEvent from "./moveToEvent/App"
+import Unrelated from "./unrelated/App"
 
 export default function Home() {
     return (
@@ -21,9 +22,13 @@ const sublessons = [
         title: "Should this code move to an event handler?",
         component: MoveToEvent,
     },
+    {
+        id: 2,
+        title: "Is your Effect doing several unrelated things?",
+        component: Unrelated,
+    },
 ]
 
-// Unrelated: Is your Effect doing several unrelated things?
 // Calculate: Are you reading some state to calculate the next state?
 // NotReacting: Do you want to read a value without “reacting” to its changes?
 // Unintended: Does some reactive value change unintentionally?
