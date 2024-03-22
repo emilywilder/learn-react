88b226219a2bab34e62d7a5befea1e2a8050a833
Emily Lockhart <validitymaiden@gmail.com>
2024-03-22 18:08:15 -0400

    use <Card>

--- modules/learn-react/escape-hatches/removing/unrelated/App.jsx
+++ modules/learn-react/escape-hatches/removing/unrelated/App.jsx
@@ -1,15 +1,32 @@
 import "./utilities"
+import Card from "./Card"
 
 import Combined from "./Combined"
 import Separated from "./Separated"
 
 export default function Unrelated() {
     return (
-        <div className="h-full">
-            <div>
-                <Combined />
-                <Separated />
-            </div>
+        <div className="flex flex-col h-full items-center justify-evenly">
+            <Card>
+                <Card.Title>Combined</Card.Title>
+                <Card.Content>
+                    This example combines both fetches within the same
+                    useEffect().
+                </Card.Content>
+                <Card.Actions>
+                    <Combined />
+                </Card.Actions>
+            </Card>
+            <Card>
+                <Card.Title>Separated</Card.Title>
+                <Card.Content>
+                    This example separates each fetch into their own
+                    useEffect().
+                </Card.Content>
+                <Card.Actions>
+                    <Separated />
+                </Card.Actions>
+            </Card>
         </div>
     )
 }
