923bfee141a9ff7c9e12838faf43728327febf1e
Emily Lockhart <validitymaiden@gmail.com>
2024-03-22 15:40:29 -0400

    use basis and the now accessable h-full to put card into the top 3/4 of the page

--- components/Blank.jsx
+++ components/Blank.jsx
@@ -1,13 +1,17 @@
 export default function Blank({ text = "This app is yet to be defined." }) {
     return (
-        <div className="pt-10 flex justify-center">
-            <div className="card shadow-xl overflow-hidden">
-                <div className="w-full h-80 bg-cover bg-center bg-[url('/images/karina-vorozheeva-rW-I87aPY5Y-unsplash.jpg')]" />
-                <div className="card-body">
-                    <h2 className="card-title justify-center">
-                        Oh hai ≽^•⩊•^≼
-                    </h2>
-                    <p>{text}</p>
+        <div className="h-full w-full flex flex-col">
+            <div className="basis-3/4">
+                <div className="flex h-full items-center justify-center">
+                    <div className="card shadow-xl overflow-hidden">
+                        <div className="w-full h-80 bg-cover bg-center bg-[url('/images/karina-vorozheeva-rW-I87aPY5Y-unsplash.jpg')]" />
+                        <div className="card-body">
+                            <h2 className="card-title justify-center">
+                                Oh hai ≽^•⩊•^≼
+                            </h2>
+                            <p>{text}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
