375c84b9124a1f372407439fa766dfb1fb8c8de4
Emily Lockhart <validitymaiden@gmail.com>
2024-03-23 12:07:45 -0400

    consolidate sublesson card into <SublessonCard>

--- modules/learn-react/escape-hatches/removing/unrelated/App.jsx
+++ modules/learn-react/escape-hatches/removing/unrelated/App.jsx
@@ -1,71 +1,19 @@
-import { useState } from "react"
-
 import "./utilities"
-import Card from "./Card"
-import { ListboxSelect } from "./Forms"
+
 import Combined from "./Combined"
 import Separated from "./Separated"
-import { getCountries } from "./utilities"
 
-function CombinedCard() {
-    const countries = getCountries()
-    const [selected, setSelected] = useState(null)
-
-    return (
-        <Card>
-            <Card.Title>Combined</Card.Title>
-            <Card.Content>
-                <p>
-                    This example combines both fetches within the same
-                    useEffect().
-                </p>
-                <div className="z-20">
-                    <ListboxSelect
-                        options={countries}
-                        selected={selected || "Select a country"}
-                        setSelected={setSelected}
-                    />
-                </div>
-                <div className="z-10">
-                    <Combined key={selected} country={selected} />
-                </div>
-            </Card.Content>
-        </Card>
-    )
-}
-
-function SeparateCard() {
-    const countries = getCountries()
-    const [selected, setSelected] = useState(null)
-
-    return (
-        <Card>
-            <Card.Title>Separated</Card.Title>
-            <Card.Content>
-                <p>
-                    This example separates each fetch into their own
-                    useEffect().
-                </p>
-                <div className="z-20">
-                    <ListboxSelect
-                        options={countries}
-                        selected={selected || "Select a country"}
-                        setSelected={setSelected}
-                    />
-                </div>
-                <div className="z-10">
-                    <Separated key={selected} country={selected} />
-                </div>
-            </Card.Content>
-        </Card>
-    )
-}
+import SublessonCard from "./SublessonCard"
 
 export default function Unrelated() {
     return (
         <div className="flex flex-row h-full items-center justify-evenly">
-            <CombinedCard />
-            <SeparateCard />
+            <SublessonCard title={"Combined"} Sublesson={Combined}>
+                This example combines both fetches within the same useEffect().
+            </SublessonCard>
+            <SublessonCard title={"Separated"} Sublesson={Separated}>
+                This example separates each fetch into their own useEffect().
+            </SublessonCard>
         </div>
     )
 }
---
+++ modules/learn-react/escape-hatches/removing/unrelated/SublessonCard.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+import Card from "./Card"
+import { ListboxSelect } from "./Forms"
+import { getCountries } from "./utilities"
+
+export default function SublessonCard({ title, Sublesson, children }) {
+    const countries = getCountries()
+    const [selected, setSelected] = useState(null)
+
+    return (
+        <Card>
+            <Card.Title>{title}</Card.Title>
+            <Card.Content>
+                <p>{children}</p>
+                <div className="z-20">
+                    <ListboxSelect
+                        options={countries}
+                        selected={selected || "Select a country"}
+                        setSelected={setSelected}
+                    />
+                </div>
+                <div className="z-10">
+                    <Sublesson key={selected} country={selected} />
+                </div>
+            </Card.Content>
+        </Card>
+    )
+}
