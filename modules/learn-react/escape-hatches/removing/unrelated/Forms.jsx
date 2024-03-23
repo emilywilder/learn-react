529dd4d16889808073aea008e312f9fc6bd8634c
Emily Lockhart <validitymaiden@gmail.com>
2024-03-23 19:52:03 -0400

    change amber to blue in listbox and remove unused css classes

--- modules/learn-react/escape-hatches/removing/unrelated/Forms.jsx
+++ modules/learn-react/escape-hatches/removing/unrelated/Forms.jsx
@@ -66,7 +66,7 @@ export function ListboxSelect({ options, selected, setSelected, fetching }) {
     return (
         <Listbox value={selected} onChange={setSelected}>
             <div className="relative mt-1">
-                <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none sm:text-sm">
                     <span className="block truncate">{selected}</span>
 
                     <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
@@ -92,7 +92,7 @@ export function ListboxSelect({ options, selected, setSelected, fetching }) {
                                 className={({ active }) =>
                                     `relative cursor-default select-none py-2 pl-10 pr-4 ${
                                         active
-                                            ? "bg-amber-100 text-amber-900"
+                                            ? "bg-blue-100 text-blue-900"
                                             : "text-gray-900"
                                     }`
                                 }
@@ -110,7 +110,7 @@ export function ListboxSelect({ options, selected, setSelected, fetching }) {
                                             {option}
                                         </span>
                                         {selected ? (
-                                            <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                            <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-blue-600">
                                                 <CheckIcon
                                                     className="h-5 w-5"
                                                     aria-hidden="true"
