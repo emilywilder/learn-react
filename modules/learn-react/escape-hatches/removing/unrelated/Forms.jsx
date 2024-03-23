e58c6bbd1737298d493755fe271a7aed3f3831b8
Emily Lockhart <validitymaiden@gmail.com>
2024-03-23 17:28:07 -0400

    somehow lost my change to use a comma rather than 'in'

--- modules/learn-react/escape-hatches/removing/unrelated/Forms.jsx
+++ modules/learn-react/escape-hatches/removing/unrelated/Forms.jsx
@@ -34,7 +34,7 @@ export default function ShippingFormRender({
             <div className="z-10">
                 {Array.isArray(areas) && areas.length > 0 ? (
                     <BarChart
-                        title={`Areas of ${city} in ${country}`}
+                        title={`Areas of ${city}, ${country}`}
                         dataset={areas}
                     />
                 ) : (
