3ca5ba1293bee9e5a61c55a0c2858edd4e85cdbc
Emily Lockhart <validitymaiden@gmail.com>
2024-03-23 19:10:07 -0400

    add more of a description for each card

--- modules/learn-react/escape-hatches/removing/unrelated/App.jsx
+++ modules/learn-react/escape-hatches/removing/unrelated/App.jsx
@@ -10,9 +10,14 @@ export default function Unrelated() {
         <div className="flex flex-row h-full items-center justify-evenly space-x-4 m-4">
             <SublessonCard title={"Combined"} Sublesson={Combined}>
                 This example combines both fetches within the same useEffect().
+                <br />
+                When a city is changed, both the cities and areas are fetched
+                rather that just the areas.
             </SublessonCard>
             <SublessonCard title={"Separated"} Sublesson={Separated}>
                 This example separates each fetch into their own useEffect().
+                <br />
+                When a city is changed, only the areas are fetched.
             </SublessonCard>
         </div>
     )
